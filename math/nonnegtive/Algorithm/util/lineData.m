a2caffef3a3982889d91e5da6ef1c31ee9ea9515
libin <libin@sohu.com>
2020-02-26 17:02:10 +0800

    2020年02月26日

--- math/nonnegtive/Algorithm/util/lineData.m
+++ math/nonnegtive/Algorithm/util/lineData.m
@@ -2,26 +2,28 @@ function d=lineData(A,b,x,y)
 [m,n]=size(A);
 d=repmat([x(1),y(1),x(2),y(2)],[m,1]);
 for i=1:m
-    if abs(A(i,1))>1e-8
+    if abs(A(i,1))>1e-8 && abs(A(i,2))>1e-8
         d(i,1)=(b(i,1)-A(i,2)*y(1))/A(i,1);
-    else
-        d(i,1)=1;
-        if abs(A(i,2))>1e-8
-            d(i,2)=(b(i,1)-A(i,1)*x(1))/A(i,2);
-        else
-            print('zores')
-        end
-    end
-    if abs(A(i,2))>1e-8
+        d(i,2)=(b(i,1)-A(i,2)*y(2))/A(i,1);
+        d(i,3)=(b(i,1)-A(i,1)*x(1))/A(i,2);
         d(i,4)=(b(i,1)-A(i,1)*x(2))/A(i,2);
-    else
-        d(i,4)=1;
-        if abs(A(i,1))>1e-8
-            d(i,3)=(b(i,1)-A(i,2)*y(1))/A(i,1);
-        else
-            print('zores')
-        end
-    end
+        dd=[d(i,1),y(1);d(i,2),y(2);x(1),d(i,3);x(2),d(i,4)];
+        dd=sortrows(dd,1);
+        d(i,1)=dd(2,1);
+        d(i,2)=dd(3,1);
+        d(i,3)=dd(2,2);
+        d(i,4)=dd(3,2);
+    elseif abs(A(i,1))>1e-8
+        d(i,1)=b(i,1)/A(i,1);
+        d(i,2)=b(i,1)/A(i,1);
+        d(i,3)=y(1);
+        d(i,4)=y(2);   
+    elseif abs(A(i,2))>1e-8
+        d(i,1)=x(1);
+        d(i,2)=x(2);
+        d(i,3)=b(i,1)/A(i,2);
+        d(i,4)=b(i,1)/A(i,2);   
+    end  
 end
 
 % valid data
