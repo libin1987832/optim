33a398ba8ea4179cfce0f731885bb9fe119c21b2
home <home@lb.com>
2020-02-26 22:54:48 +0800

    issue:finish draw2method

--- math/nonnegtive/Algorithm/egdraw2method.m
+++ math/nonnegtive/Algorithm/egdraw2method.m
@@ -14,5 +14,18 @@ b=[1;1;-0.5;-2];
 d=lineData(A,b,[-2,1],[-1.5,3]);
 line(d(:,1:2)',d(:,3:4)')
 line([-2/3,1],[2/3,-1],'LineStyle','--');
-
+hold on
+for x=-2:0.1:1
+    for y=-1.5:0.2:3
+        x0=[x;y];
+        [xk,r0,rk,fkFM,fm,fr]=FM(x0,q,r,A,b);
+        [xk,rk,fkssqr,f0,lambe]=ssqr(x0,A,b);
+    if  fkFM>fkssqr*1.0001
+        plot(x,y,'ro');
+    else
+        plot(x,y,'b+');
+    end
+    hold on
+    end
+end
 
