92bfe7c70f9ba58f93f28da09da55cfb1cc5d84c
libin <libin@sohu.com>
2020-02-27 11:01:47 +0800

    2020年02月27日

--- math/nonnegtive/Algorithm/egdraw2method.m
+++ math/nonnegtive/Algorithm/egdraw2method.m
@@ -15,17 +15,26 @@ d=lineData(A,b,[-2,1],[-1.5,3]);
 line(d(:,1:2)',d(:,3:4)')
 line([-2/3,1],[2/3,-1],'LineStyle','--');
 hold on
+xx1=[];
+yy1=[];
+xx2=[];
+yy2=[];
+
 for x=-2:0.1:1
     for y=-1.5:0.2:3
         x0=[x;y];
         [xk,r0,rk,fkFM,fm,fr]=FM(x0,q,r,A,b);
         [xk,rk,fkssqr,f0,lambe]=ssqr(x0,A,b);
     if  fkFM>fkssqr*1.0001
-        plot(x,y,'ro');
+        xx1=[xx1,x];
+        yy1=[yy1,y];
     else
-        plot(x,y,'b+');
+       xx2=[xx2,x];
+        yy2=[yy2,y];
     end
-    hold on
     end
 end
+plot(xx1,yy1,'ro');
+hold on 
+plot(xx2,yy2,'b+');
 
