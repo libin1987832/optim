b90f2b04700a6f742f657b051f2ea21528b7a12d
libin <libin@sohu.com>
2020-03-11 22:37:57 +0800

    2020年03月11日

--- math/nonnegtive/Algorithm/egQQFM.m
+++ math/nonnegtive/Algorithm/egQQFM.m
@@ -5,7 +5,8 @@ q1=[1/sqrt(39);-1/sqrt(39);1/sqrt(39);6/sqrt(39)];
 q2=[19;-19;-20;-3]/sqrt(1131);
 Q=[q1,q2];
 
-x=[-3;-1];
+% x=[-3;-1];
+x=[-3;1]/4;
 r0=b-A*x;
 r0A=r0;
 r0A(r0A>0)=1;
@@ -17,16 +18,18 @@ IQ=diag([1,1,1,1])-Q*Q'*NK;
 
 % matlab in inexact arithmetic
 [q,r]=qr(A);
-
+for i=1:10
 [xk,r0,rk,fkFM,fm,fr]=FM(x,q,r,A,b);
-% r_{k}
-r0IQ=b-A*x;
-% r_{k+1}
-rkIQ=IQ*r0IQ;
-rkIQS=rkIQ;
-rkIQS(rkIQ>0)=1;
-rkIQS(rkIQ<0)=0;
-% N_{k+1}
-NK2=diag(rkIQS)
+x=xk;
+end
+% % r_{k}
+% r0IQ=b-A*x;
+% % r_{k+1}
+% rkIQ=IQ*r0IQ;
+% rkIQS=rkIQ;
+% rkIQS(rkIQ>0)=1;
+% rkIQS(rkIQ<0)=0;
+% % N_{k+1}
+% NK2=diag(rkIQS)
 % r_{k+1}=b-Ax_{k+1}
 rkIQ2=b-A*xk;
