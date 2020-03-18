d9fd14113e1eaf32881b89673343785839c31ce3
libin <libin@sohu.com>
2020-03-18 16:01:49 +0800

    2020年03月18日

--- math/nonnegtive/Algorithm/Untitled.m
+++ math/nonnegtive/Algorithm/Untitled.m
@@ -1,5 +1,16 @@
+% A=rand(4,2)*10;
+% [q,r]=qr(A);
+% q=q(:,[1,2]);
+% Aq=q*q'*diag([1,1,0,0])
+% [f,v]=eig(Aq)
 A=rand(4,2)*10;
-[q,r]=qr(A);
+As=sym(floor(A));
+[q,r]=qr(As);
 q=q(:,[1,2]);
 Aq=q*q'*diag([1,1,0,0]);
-[f,v]=eig(Aq)
+Aq;
+[v,j]=jordan(B);
+lj=logical(vpa(j>0.99991));
+jj=diag([lj(1,1),lj(2,2),lj(3,3),lj(4,4)]);
+vpa(v*jj*inv(v))
+% [f,v]=eig(Aq)
