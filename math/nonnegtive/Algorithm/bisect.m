bedd58a172d40894db95a9f9d39e43715ad27f69
home <home@lb.com>
2020-03-19 21:07:34 +0800

    2020年03月19日

--- math/nonnegtive/Algorithm/bisect.m
+++ math/nonnegtive/Algorithm/bisect.m
@@ -2,7 +2,7 @@ function c=bisect(r,p)
 TOL = 1e-10;
 a=0;
 b=1;
-while f(r,p,b)>0
+while f(r,p,b)<0
     b=b/2;
 end
 while (b-a)/2>TOL
@@ -21,4 +21,16 @@ function v=f(r,p,a)
     rp=r-a*p;
     rp(rp<0)=0;
     v=p'*rp;
+end
+function [am,bm]=sec(r,p,a,b)
+c=(a+b)/2;
+if f(r,p,c)>0
+    am=a;
+    bm=c;
+    return;
+else
+    [am,bm]=sec(a,c);
+    [am,bm]=sec(c,b);
+    return;
+end
 end
