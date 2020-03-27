55dee72a4c3d502ca3a30b50d4a982b00aab7e5a
home <home@lb.com>
2020-03-27 20:08:16 +0800

    2020年03月27日

--- math/nonnegtive/Algorithm/getBn.m
+++ math/nonnegtive/Algorithm/getBn.m
@@ -1,17 +1,21 @@
-function sum=getBn(QQ,r,I)
+function sum2=getBn(QQ,r,I)
 % Nk=r;
 % Nk(Nk>0)=1;
 % Nk(Nk<0)=0;
 % Bn=I-QQ*diag(Nk);
-% rkn=Bn*r;
+% rkn2=Bn*r;
 [m,n]=size(r);
 rkn=r;
 p=find(rkn>0);
 sum=0;
+sum2=0;
 for i=1:m
     rkn(i)=r(i)-QQ(i,p)*r(p);
-    sum=sum+sign(rkn(i)*r(i));
-%     if sign(rkn(i)*r(i))>0
-%         sum=sum+1;
+%     sum=sum+sign(rkn(i)*r(i));
+    if sign(rkn(i)*r(i))>0
+        sum2=sum2+1;
+    end
+%     if sum2~=sum
+%         error('dddd')
 %     end
 end
