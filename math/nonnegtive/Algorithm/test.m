92c813e12fe8b7f2b110f1f9129522167635123c
home <home@lb.com>
2020-04-17 18:08:04 +0800

    2020年04月17日

--- math/nonnegtive/Algorithm/test.m
+++ math/nonnegtive/Algorithm/test.m
@@ -1,7 +1,15 @@
-A=diag([10^15,1]);
-% tf = issymmetric(A);
-
-d = eig(A);
-tol=length(d)*eps(max(d))
-isposdef = all(d > tol)
-issemidef = all(d > -tol)
+% A=diag([10^15,1]);
+% % tf = issymmetric(A);
+% 
+% d = eig(A);
+% tol=length(d)*eps(max(d))
+% isposdef = all(d > tol)
+% issemidef = all(d > -tol)
+% 
+% 
+n=5;
+A=rand(n+2,n);
+[q,r]=qr(A);
+Q1=q(:,1:3);
+N=diag([1,1,0,0,1,1,1])
+diag(ones(7,1))-Q1*Q1'*N
