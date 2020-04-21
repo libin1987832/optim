c339ec459b93f412cfeefac101600dc30d93027c
home <home@lb.com>
2020-04-21 22:55:02 +0800

    text

--- math/nonnegtive/Algorithm/example_paper/egPredictionPoint.m
+++ math/nonnegtive/Algorithm/example_paper/egPredictionPoint.m
@@ -1,9 +1,9 @@
 addpath('../FM')
 addpath('../hybrid')
-
+addpath('../util')
 A=[1,1;-1,-1;1,0;6,3];
 b=[1;1;-0.5;-2];
-[q,r]=qr(A);
+[Q,r]=qr(A);
 Qn=Q(:,1:2);
 nIter=10;
 QQ=nIter*Qn*Qn';
@@ -20,12 +20,19 @@ xx1=[];
 yy1=[];
 xx2=[];
 yy2=[];
-
+I=diag([1,1,1,1]);
 for x=-2:0.1:1
     for y=-1.5:0.2:3
         x0=[x;y];
-        fm=b-Ax0;
+        fm=b-A*x0;
         ssign=getBn(QQ,fm,I);
+        if ssign==4
+            xx1=[xx1;x];
+            yy1=[yy1;y];
+        else
+            xx2=[xx2;x];
+            yy2=[yy2;y];
+        end
     end
 end
 % express neton's method good
