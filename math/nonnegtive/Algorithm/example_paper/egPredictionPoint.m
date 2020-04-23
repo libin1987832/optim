36c97a370d8386b3f155f72b22dce72b2c4a5f05
libin <libin@sohu.com>
2020-04-23 17:01:57 +0800

    2020年04月23日

--- math/nonnegtive/Algorithm/example_paper/egPredictionPoint.m
+++ math/nonnegtive/Algorithm/example_paper/egPredictionPoint.m
@@ -3,13 +3,12 @@ addpath('../hybrid')
 addpath('../util')
 A=[1,1;-1,-1;-1,0;6,3];
 b=[1;1;0.5;-2];
+[sm,sn]=size(A);
 [Q,r]=qr(A);
-Qn=Q(:,1:2);
-nIter=5;
+Qn=Q(:,1:sn);
 QQn=Qn*Qn';
+nIter=3;
 QQ=nIter*QQn;
-x=[-1;0.05;0];
-y=[1;0.05:0];
 A=[1,1;-1,-1;-1,0;-6,-3];
 b=[1;1;0.5;2];
 % output
@@ -21,28 +20,31 @@ xx1=[];
 yy1=[];
 xx2=[];
 yy2=[];
-I=diag([1,1,1,1]);
- for x=-2:0.1:1
-     for y=-1.5:0.2:3
-        x0=[x;y];
+xxx=[];
+%  for x=-2:0.1:1
+%      for y=-1.5:0.2:3
+x=-0.6;
+y=0.7;
+x0=[x;y];
         fm=b-A*x0;
         ssign=getBn(QQ,fm,I);
-        if ssign==4
+        if ssign==sm
             xx1=[xx1;x];
             yy1=[yy1;y];
         else
             xx2=[xx2;x];
             yy2=[yy2;y];
         end
-         x00=x0;
-         for i=0:5
-            [xk,r0,rk,fk,fm,fr]=FM(x00,Q,R,A,b);
-            x00=xk;
-         end
-         Bn=I-QQn*diag(Nk);
-         r10=Bn^2*rkn
-    end
-end
+        [optimaln,real]=testReason(x0,A,b,nIter);
+        if optimaln==nIter
+            xxx=[xxx [x0;5]];
+        elseif real==nIter
+            xxx=[xxx [x0;optimaln]];
+        else
+            xxx=[xxx [x0;-1]];
+        end
+%     end
+% end
 % express neton's method good
 plot(xx1,yy1,'r*');
 hold on 
---
+++ math/nonnegtive/Algorithm/example_paper/testReason.m
@@ -0,0 +1,46 @@
+% 检测预测效果 返回1 积极集不固定 无法预测 返回2 估计值错误 real FM goundtrueth optimal estimate max
+function [optimaln,real]=testReason(x00,A,b,n)
+same=n;
+[sm,sn]=size(A);
+[Q,R]=qr(A);
+Qn=Q(:,1:sn);
+QQn=Qn*Qn';
+rkn=b-A*x00;
+Nk=rkn;
+Nk(Nk>0)=1;
+Nk(Nk<0)=0;
+for i=1:n
+    [xk,r0,rk,fk,fm,fr]=FM(x00,Q,R,A,b);
+    x00=xk;
+    rkn0=b-A*x00;
+    Nk0=rkn0;
+    Nk0(Nk0>0)=1;
+    Nk0(Nk0<0)=0;
+    % active set change break；
+    if ~logical(Nk0==Nk)
+        same=i-1;
+        break;
+    end
+end
+real=same;
+if same > 0
+    while(predict(QQn,A,b,x00,same,rkn,Nk)==-1&& same >0)
+        same=same-1;
+    end
+    optimaln=same;
+end
+
+function r=predict(QQn,A,b,x0,n,rkn,Nk)
+[sm,sn]=size(b);
+I=diag(ones(sm,1));
+Bn=I-QQn*diag(Nk);
+rt=Bn^n*rkn;
+rp=(I-n*QQn*diag(Nk))*rkn;
+for i=1:sm
+    if sign(rt(i))*sign(rp(i))<0
+        r=-1;
+        return
+    end
+end
+r=1;
+return
