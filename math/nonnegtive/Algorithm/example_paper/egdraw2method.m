d5b1bb4e8c2e06ec5c0beabc3b7424422730714c
libin <libin@sohu.com>
2020-04-26 16:01:45 +0800

    2020年04月26日

--- math/nonnegtive/Algorithm/example_paper/egdraw2method.m
+++ math/nonnegtive/Algorithm/example_paper/egdraw2method.m
@@ -1,29 +1,34 @@
 % compare simple iterator and newton method 
-A=[1,1;-1,-1;1,0;6,3];
-b=[1;1;-0.5;-2];
+% A=[1,1;-1,-1;1,0;6,3];
+% b=[1;1;-0.5;-2];
+addpath('FM');
+addpath('util');
+addpath('hybrid');
+A=[1,1;-1,-1;-1,0;-6,-3];
+b=[1;1;0.5;2];
 
 [q,r]=qr(A);
 
-x=[-1;0.05;0];
-y=[1;0.05:0];
-
-
-A=[1,1;-1,-1;1,0;6,3];
-b=[1;1;-0.5;-2];
 % output
-d=lineData(A,b,[-2,1],[-1.5,3]);
+d=lineData(A,b,[-2.5,0],[-1.5,3]);
 line(d(:,1:2)',d(:,3:4)')
-line([-2/3,1],[2/3,-1],'LineStyle','--');
+
+line([-2/3,-2.5],[2/3,2.5],'LineStyle','--');
 hold on
 xx1=[];
 yy1=[];
 xx2=[];
 yy2=[];
 
-for x=-2:0.1:1
+for x=-2.5:0.1:0
     for y=-1.5:0.2:3
+        
         % form a point
         x0=[x;y];
+        % in the expose face 
+        %x0=[-2.5;1.7]
+        % not in
+        %x0=[-2;0];
         xF=x0;
         % two step compare
         for j=1:2
@@ -33,11 +38,12 @@ for x=-2:0.1:1
         [xk,rk,fkssqr,f0,lambe]=ssqr(x0,A,b);
         % if the reduction is more ,then add the point
     if  fkFM>fkssqr
-        xx1=[xx1,x];
-        yy1=[yy1,y];
+        % newtow good
+        xx1=[xx1, x0(1)];
+        yy1=[yy1, x0(2)];
     else
-        xx2=[xx2,x];
-        yy2=[yy2,y];
+        xx2=[xx2,x0(1)];
+        yy2=[yy2,x0(2)];
     end
     end
 end
