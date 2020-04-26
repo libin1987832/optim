1a4ca12b35cc895a36f0d2e043a5756eb19e01ef
libin <libin@sohu.com>
2020-04-26 15:01:59 +0800

    2020年04月26日

--- math/nonnegtive/Algorithm/example_paper/compare.m
+++ math/nonnegtive/Algorithm/example_paper/compare.m
@@ -1,16 +1,19 @@
+addpath('FM');
+addpath('util');
+addpath('hybrid');
 A=[1,1;-1,-1;-1,0;-6,-3];
 b=[1;1;0.5;2];
-x0=[-1;-0.5];
+x0=[-1;-0.2];
 [Q,R]=qr(A);
-xx1=[x0(1)];
-yy1=[x0(2)];
+xx1=[];
+yy1=[];
 error=[];
-for i=0:3
+for i=0:1
 fk=b-A*x0;
 fk(fk<0)=0;
 error=[error;norm(A'*(fk))];
-xx1=[xx1;xk(1)];
-yy1=[yy1;xk(2)];
+xx1=[xx1;x0(1)];
+yy1=[yy1;x0(2)];
 [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
 x0=xk;
 end
@@ -19,13 +22,14 @@ yyF=yy1;
 errorF=error;
 Qn=Q(:,1:2);
 QQ=3*Qn*Qn';
+I=diag(ones(4,1))
 ssign=getBn(QQ,fm,I);
 
-[xk,rk,fk,f0,lambe]=ssqr(x0,A,b);
-errorN=[error;norm(A'*rk)];
-xx1=[xx1;xk(1)];
-yy1=[yy1;xk(2)];
-for i=0:10
+[xkN,rkN,fk,f0,lambe]=ssqr(x0,A,b);
+errorN=[error;norm(A'*rkN)];
+% xx1=[xx1;xk(1)];
+% yy1=[yy1;xk(2)];
+for i=0:3
 fk=b-A*x0;
 fk(fk<0)=0;
 errorF=[errorF;norm(A'*(fk))];
@@ -36,17 +40,21 @@ x0=xk;
 end
 
 % plot(4:size(errorF,1),errorF(4:end,1),'-b*',4:size(errorN,1),errorN(4:end),'-or');
-d=lineData(A,b,[-2,1],[-1.5,3]);
+d=lineData(A,b,[-2.5,0],[-1.5,3]);
 line(d(:,1:2)',d(:,3:4)')
 % line([-2/3,1],[2/3,-1],'LineStyle','--');
-line([-2/3,-2],[2/3,2],'LineStyle','--');
+line([-2/3,-2.5],[2/3,2.5],'LineStyle','--');
 hold on 
-line(xx1',yy1')
-plot(xx1,yy1,'r*');
-for i=1:size(xx1,1)
-    x0=[xx1(i),yy1(i)];
+line(xxF',yyF','LineWidth',2)
+plot(xxF,yyF,'r*');
+H=line([xx1;xkN(1)]',[yy1;xkN(2)]','Color',[.1 .1 .1],'LineWidth',2);
+plot([xx1;xkN(1)]',[yy1;xkN(2)]','bo');
+line([xx1;]',[yy1;]','Color',[.5 0 0],'LineWidth',2);
+    text(xkN(1),xkN(2),'3')
+for i=1:size(xxF,1)
+    x0=[xxF(i),yyF(i)];
     c=num2str(i);
-    plot(x0(1),x0(2),'*')
+%     plot(x0(1),x0(2),'*')
     c=[' ',c];
     text(x0(1),x0(2),c)
     %plot(xx1,yy1,'r*');
