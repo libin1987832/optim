b9beabc0f9cac67d74308ca287f4a7e88573d0d7
home <home@lb.com>
2020-05-20 06:49:14 +0800

    modify

--- math/lcp/computDLU.m
+++ math/lcp/computDLU.m
@@ -1,7 +1,7 @@
-function xk=computDLU(A,d,x0)
+function xk=computDLU(A,x0)
 [m,n]=size(A);
 xk=zeros(n,1);
 for j=1:n
-    xk(j)=x0(j)-(d(j)+A(j,:)*[xk(1:(j-1));x0(j:n)])/A(j,j);
+    xk(j)=x0(j)-(A(j,:)*[xk(1:(j-1));x0(j:n)])/A(j,j );
 end
 %
--- math/lcp/predict.m
+++ math/lcp/predict.m
@@ -1,11 +1,9 @@
 function [x]=predict(A,x0,q)
 [m,n]=size(A);
 b=computAC(A,q);
-rq=comput
-for i=1:m
-    for j=1:i-1
-        bd=bd+A(i,j)*b(j);
-    end
-    b(i)=(q(i)-bd)/A(i,i);
+rq=b;
+for i=1:3
+rq=computDLU(C,rq);
+rq=rq+b;
 end
-C=
+x=rq;
