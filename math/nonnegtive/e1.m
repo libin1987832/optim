63c654313f9e7d940ef59ec5f64c063a7001b10e
libin <libin@sohu.com>
2020-05-22 15:02:20 +0800

    2020-05-22

--- math/nonnegtive/e1.m
+++ math/nonnegtive/e1.m
@@ -1,4 +1,5 @@
 clear
+addpath(genpath(pwd));
 A=[1,1;-1,-1;-1,0];
 b=[1;1;1/2];
 x=[-1;-1];
@@ -6,4 +7,6 @@ r=b-A*x;
 r(r<0)=0;
 AA=A'*A;
 Ar=A'*r;
-AA/Ar
+AA\Ar
+[Q,R]=qr(A);
+[x11,f1]=FM(x,Q,R,A,b);
