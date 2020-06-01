085401049c0ae6c41c0139d068e92971b932216c
libin <libin@sohu.com>
2020-06-01 15:01:39 +0800

    2020-06-01

--- math/lcp/asymsub/asysub.m
+++ math/lcp/asymsub/asysub.m
@@ -1,35 +1,36 @@
 function x0=asysub(x0,M,q,nmax,etc,ete,trr,trmax,rou)
 funfi0=funfi(x0,M,q);
 funfim0=max(funfi0,1e5);
-nf=5;
-ns=3;
+nf=2;
+ns=2;
 tr0=(trr+trmax)/2;
+iter=0;
 for i=1:nmax
-    [xfn,cmax] = splitS(M,q,1,x0,nf-1,cmax);
-    [xfn1,cmax] = splitS(M,q,1,xfn,1,cmax);
-    xs=subspacesearch(xfk,M,q);
-    xsn=norm(xs-xk);
+    cmax=0;
+    iter=iter+nf+ns;
+   [xfA,cmax] = splitS(M,q,1,x0,nf,cmax);
+    xs=subspacesearch(xfA(:,nf),M,q);
+    xsn=norm(xs-xfA(:,nf));
     mtr=min(1,tr0/xsn);
-    xi=xi+mtr*(xs-xk);
-    [xsk1,cmax] = splitS(M,q,1,xi,1,cmax);
-    [xsk2,cmax] = splitS(M,q,1,xsk1,1,cmax);
-    [xsk3,cmax] = splitS(M,q,1,xsk2,ns-1,cmax);
+    xi=xfA(:,nf)+mtr*(xs-xfA(:,nf));
+    xi=max(0,xi);
+    [xsA,cmax] = splitS(M,q,1,xi,ns,cmax);
     rok=max(rou,(1+cmax)/2);
-    p1=norm(xsk1-xfn1);
-    p2=norm(xsk2-xsk1);
-    p3=norm(xfn-xfn1);
-    funfi3=funfi(xk3,M,q);
+    p1=norm(xsA(:,1)-xfA(:,nf));
+    p2=norm(xsA(:,2)-xsA(:,1));
+    p3=norm(xfA(:,nf)-xs(:,1));
+    funfi3=funfi(xsA(:,ns),M,q);
     if p1<=rok*p3 && p2<=rok*p1
-        x0=xsk3;
+        x0=xsA(:,ns);
         tr0=median([trr,ete*tr0,trmax]);
     elseif funfi3<=0.5*funfim0
-          x0=xsk3;
+          x0=xsA(:,ns);
           funfim0=0.5*funfi3;
           tr0=median([trr,ete*tr0,trmax]);
     else
         tr0=etc*tr0;
     end
-    if funfi3<1e-6
+    if funfi3<1e-5
         break;
     end
 end
---
+++ math/lcp/asymsub/test.m
@@ -0,0 +1,22 @@
+n=1000;
+M=randn(n,n)*10^3;
+M=M-diag(diag(M));
+colM=sum(abs(M),2);
+M=M+diag(colM);
+xs=randn(n,1);
+I1=(xs<0);
+xs(I1)=0;
+I2=(xs>0);
+q=rand(n,1);
+qt=M*xs;
+q(xs>0)=-qt(xs>0);
+% degency so +0.1
+q(xs==0)=max(abs(qt))+0.1;
+nmax=500;
+etc=0.5;
+ete=2;
+rou=0.99;
+trmax=1e12;
+trr=1;
+x0=ones(n,1)*100;
+y0=asysub(x0,M,q,nmax,etc,ete,trr,trmax,rou);
