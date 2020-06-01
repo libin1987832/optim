ebd82f5b30e9ef2fd9065caf57337a4461fe83ad
home <home@lb.com>
2020-06-01 20:07:57 +0800

    2020-06-01

--- math/lcp/ours/predict/Untitled2.m
+++ math/lcp/ours/predict/Untitled2.m
@@ -1,8 +1,14 @@
 n=3;
-A=rand(n)*1000;
-xs=rand(n,1)*10;
+A=rand(n);
+xs=rand(n,1);
 q=A*xs;
 A\q
 xs
-[x,n,xa]=guaseidel(A,q,ones(n,1)*10,1.0e-5,50);
-x
+D = diag(diag(A));%求A的对角矩阵
+L = tril(A,-1);%求A的下三角矩阵,不带对角线
+U = triu(A,1);%求A的上三角矩阵
+DLU=-inv(D+L)*U;
+eig(DLU)
+[x,n,xa]=guaseidel(A,q,zeros(n,1),1e-5,10);
+xa;
+x;
--- math/lcp/ours/predict/guaseidel.m
+++ math/lcp/ours/predict/guaseidel.m
@@ -1,12 +1,12 @@
-function [x,n,xA] = guaseidel(A,b,x0,eps,it_max)
+function [xk,n,xA] = guaseidel(A,b,x0,eps,it_max)
 % 求线性方程组的Gauss-Seidel迭代法，调用格式为
- 
+
 %  [x, k] = guaseidel(A,b,x0,eps,it_max)
- 
+
 %  其中, A 为线性方程组的系数矩阵，b 为常数项，eps 为精度要求，默认为1e-5,
- 
+
 %  it_max 为最大迭代次数，默认为100
- 
+
 %  x 为线性方程组的解，k迭代次数
 if nargin == 3
     eps = 1.0e-6
@@ -18,21 +18,30 @@ elseif nargin <3
     return;
 end
 xA=[];
-D = diag(diag(A));%求A的对角矩阵
-L = -tril(A,-1);%求A的下三角矩阵,不带对角线
-U = -triu(A,1);%求A的上三角矩阵
-G = (D-L)\U;
-f = (D-L)\b;
-x = G*x0+f;
+% D = diag(diag(A));%求A的对角矩阵
+% L = -tril(A,-1);%求A的下三角矩阵,不带对角线
+% U = -triu(A,1);%求A的上三角矩阵
+% G = (D-L)\U;
+% f = (D-L)\b;
+% x = G*x0+f;
+[ms,ns]=size(A);
 n=1;  %迭代次数
-while norm(x-x0)>=eps
-    x0 = x;
-    x = G*x0+f;
-    xA=[xA x];
+s=1;
+%while norm(xk-x0)>=eps
+%     x0 = x;
+%     x = G*x0+f;
+for i=1:it_max
+    xk=zeros(ns,1);
+    for j=1:ns
+        sd=s*(b(j)-A(j,:)*[xk(1:(j-1));x0(j:ns)])/A(j,j);
+        xk(j)=x0(j)-sd;
+    end
+    x0=xk;
+    xA=[xA xk];
     n = n+1;
     if(n>=it_max)
         disp('Warning:迭代次数太多,可能不收敛');
         return;
     end
 end
- 
+
