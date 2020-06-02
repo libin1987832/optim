ac9543e416c4d0b95695b42eb9e18d32d568846d
home <home@lb.com>
2020-06-02 18:07:55 +0800

    2020-06-02

--- math/lcp/ours/predict/Untitled2.m
+++ math/lcp/ours/predict/Untitled2.m
@@ -1,8 +1,8 @@
 n=3;
-% A=rand(n);
-% xs=rand(n,1);
-% save('tt','A','xs');
-load('tt','A','xs')
+A=rand(n);
+xs=rand(n,1);
+save('tt1w','A','xs');
+% load('tt','A','xs')
 q=A*xs;
 A\q
 xs
@@ -10,10 +10,11 @@ D = diag(diag(A));%求A的对角矩阵
 L = tril(A,-1);%求A的下三角矩阵,不带对角线
 U = triu(A,1);%求A的上三角矩阵
 DLU=-inv(D+L)*U;
-eig(DLU)
+
 [x,n,xa]=guaseidel(A,q,zeros(n,1),1e-5,10);
 xa;
 x;
 xad=xa(:,1:8)-xa(:,2:9);
 xro=xad(:,2:8)./xad(:,1:7);
 abs(xro)
+max(eig(DLU))
