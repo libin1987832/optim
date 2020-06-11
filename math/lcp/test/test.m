0a052cd47b6791d048ea8ad18e9a32ac8ab28994
libin <libin@sohu.com>
2020-06-11 17:01:44 +0800

    2020-06-11

--- math/lcp/test/test.m
+++ math/lcp/test/test.m
@@ -6,11 +6,12 @@ addpath('./symsub')
 addpath('../other')
 addpath('../symsub')
 
-n=500;
+n=3000;
 A=randn(n);
 A=A'*A;
 B=0.1*eye(n);
-C=A+B;
+% C=A+B;
+C=sprandsym(n,0.6,0.1);
 xs=randn(n,1);
 I1=(xs<0);
 xs(I1)=0;
@@ -27,13 +28,15 @@ save('fpi','C','xs','q','n');
 x0=ones(n,1);
 nmax=100;
 
-max_iter = 100;
+max_iter = 50;
 tol_rel  = 0.0;
 tol_abs  = 0.0;
 
 
 [xkb,s,iter,Aopt]=qp_bnd(C,q);
-[w,xka,retcode] = LCPSolve(C,q);
+%[w,xka,retcode] = LCPSolve(C,q);
+% xsa=norm(xs-xka);
+% [res1,fx1]=test_valid(C,q,xka);
 [xkpsor err iter flag convergence msg] = psor(C, q, x0, 1, max_iter, tol_rel, tol_abs, false);
 [xks,ress]=splitS(C,q,1.4,x0,10);
 nf=10;
@@ -41,7 +44,7 @@ nf=10;
 [xkpa,errpa,indexpa1,indexpa2]=PA(x0,nmax,nf,C,q);
 [xkor,error,indexor,indexNor]=hybridorigin(x0,nmax,nf,C,q);
 xsb=norm(xs-xkb);
-xsa=norm(xs-xka);
+
 xspsor=norm(xs-xkpsor);
 xs2=norm(xs-xk2);
 xspa=norm(xs-xkpa);
@@ -49,11 +52,15 @@ xsor=norm(xs-xkor);
 [ress,fxs]=test_valid(C,q,xs);
 [resss,fxss]=test_valid(C,q,xks);
 [res,fx]=test_valid(C,q,xkb);
-[res1,fx1]=test_valid(C,q,xka);
+
 [res2,fx2]=test_valid(C,q,xkpsor);
 [res3,fx3]=test_valid(C,q,xk2);
 [res4,fx4]=test_valid(C,q,xkpa);
 [resor,fxor]=test_valid(C,q,xkor);
+disp(['RFNP:(',num2str(index2*(nf+1)),' ',num2str(index2),') err:',num2str(res3)])
+disp(['PA:(',num2str(indexpa1),' ',num2str(indexpa2),') err:',num2str(res4)])
+disp(['our:(',num2str(indexor),' ',num2str(indexNor),') err:',num2str(resor)])
+disp(['GS:(',num2str(max_iter),' ',num2str(0),') err:',num2str(res2)])
 %% test qp_bnd
 % n=3;
 % A=randn(n)
