e375175c2d38317d2ed60c4a8374b3f0ad8acb67
home <home@lb.com>
2020-06-14 08:10:06 +0800

    2020-06-14

---
+++ math/lcp/eg/test_rand_density.m
@@ -0,0 +1,76 @@
+%% test split
+clc
+clear
+addpath('../other')
+addpath('../symsub')
+addpath('../util')
+n=1000;
+A=randn(n);
+A=A'*A;
+B=0.1*eye(n);
+C=A+B;
+xs=randn(n,1);
+I1=(xs<0);
+xs(I1)=0;
+I2=(xs>0);
+cs2=sum(I2);
+q=rand(n,1);
+qt=C*xs;
+q(xs>0)=-qt(xs>0);
+q(xs==0)=max(abs(qt))+0.1;
+save('fpi','C','xs','q','n');
+% load('fpiq')
+% convergence of the iteratve methods
+CC=triu(C,1);
+TC=tril(C);
+[symmetric2,posdef2]=isPosdef(TC-CC);
+if posdef2==0
+disp('the iterative methods may not converge');
+else
+disp('the iterative methods converges');
+end
+% object value
+ressvvf=func(C,q,xs);
+% KKT error
+ressvv=test_valid(C,q,xs);
+% posdef
+[symmetric,posdef]=isPosdef(C);
+if posdef==0
+disp('C may not the prositive semidefine');
+else if posdef==1
+    disp('positive');
+else
+    disp('semipositive');
+    end
+end
+
+x0=ones(n,1);
+nmax=100;
+
+max_iter = 50;
+tol_rel  = 0.0;
+tol_abs  = 0.0;
+
+[xkpsor err iter flag convergence msg] = psor(C, q, x0, 1, max_iter, tol_rel, tol_abs, false);
+[xks,ress]=splitS(C,q,1.4,x0,10);
+nf=10;
+[xk2,err,index2]=splitForlcp(x0,nmax,nf,C,q);
+[xkpa,errpa,indexpa1,indexpa2]=PA(x0,nmax,nf,C,q);
+[xkor,error,indexor,indexNor]=hybridorigin(x0,nmax,nf,C,q);
+
+xspsor=norm(xs-xkpsor);
+xs2=norm(xs-xk2);
+xspa=norm(xs-xkpa);
+xsor=norm(xs-xkor);
+[ress,fxs]=test_valid(C,q,xs);
+[resss,fxss]=test_valid(C,q,xks);
+% [res,fx]=test_valid(C,q,xkb);
+  
+[res2,fx2]=test_valid(C,q,xkpsor);
+[res3,fx3]=test_valid(C,q,xk2);
+[res4,fx4]=test_valid(C,q,xkpa);
+[resor,fxor]=test_valid(C,q,xkor);
+disp(['RFNP:(',num2str(index2*(nf+1)),' ',num2str(index2),') err:',num2str(res3)])
+disp(['PA:(',num2str(indexpa1),' ',num2str(indexpa2),') err:',num2str(res4)])
+disp(['our:(',num2str(indexor),' ',num2str(indexNor),') err:',num2str(resor)])
+disp(['GS:(',num2str(max_iter),' ',num2str(0),') err:',num2str(res2)])
