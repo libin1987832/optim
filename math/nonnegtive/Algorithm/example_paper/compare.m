85d08514c53786ca7f8188f083bbaa028558de12
home <home@lb.com>
2020-06-23 19:08:18 +0800

    2020-06-23

--- math/nonnegtive/Algorithm/example_paper/compare.m
+++ math/nonnegtive/Algorithm/example_paper/compare.m
@@ -8,16 +8,20 @@ x0=[-1;-0.2];
 [Q,R]=qr(A);
 xx1=[x0(1)];
 yy1=[x0(2)];
-error=[];
+fk=b-A*x0;
+fk(fk<0)=0;
+fkk1=[(fk'*fk)];
+error=[norm(A'*(fk))];
 % take two steps for both algorithm
 for i=1:1
 fk=b-A*x0;
 fk(fk<0)=0;
-error=[error;norm(A'*(fk))];
+fkk1=[fkk1;(fk'*fk)];
 [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
 x0=xk;
 xx1=[xx1;x0(1)];
 yy1=[yy1;x0(2)];
+error=[error;norm(A'*(fk))];
 end
 xxF=xx1;
 yyF=yy1;
@@ -31,13 +35,14 @@ ssign=getBn(QQ,fm,I);
 
 % take newton type method
 [xkN,fk,dh,rkk]=ssqr4(x0,A,b);
-errorN=[error;norm(A'*rkN)];
-% xx1=[xx1;xk(1)];
-% yy1=[yy1;xk(2)];
+errorN=[error;norm(A'*rkk)];
+xx1=[xx1;xkN(1)];
+yy1=[yy1;xkN(2)];
 for i=0:3
 fk=b-A*x0;
 fk(fk<0)=0;
 errorF=[errorF;norm(A'*(fk))];
+fkk1=[fkk1;(fk'*fk)];
 xxF=[xxF;xk(1)];
 yyF=[yyF;xk(2)];
 [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
@@ -71,6 +76,13 @@ for i=1:size(xxF,1)
 end
 
 [xx1';yy1']
+for i=0:2
+fprintf('$ x_%i $ & (%4.4f,%4.4f) & %4.3f & (1,2)& (%4.4f,%4.4f) & %4.3f & (1,2)\\\\\n',i,xxF(i+1),yyF(i+1),errorF(i+1)...
+    ,xx1(i+1),yy1(i+1),errorN(i+1));
+end
+for i=3:4
+fprintf('$ x_%i $ & (%4.4f,%4.4f) & %4.3f & (1,2)& - & - & -\\\\\n',i,xxF(i+1),yyF(i+1),errorF(i+1));
+end
 % [xk1,fk1,xkArr1,countF1,countN1]=hybrid6(x0,A,b,3);
 % [xk1,fk1,xkArr1,countF1,countN1]=hybrid2(x0,A,b);
 % [xk2,fk2,xkArr2,countF2,countN2]=hybrid1(x0,A,b);
