9f3021ff804ec5bf5bf71c18e3dc009e4ce0bde5
home <home@lb.com>
2020-06-24 22:07:46 +0800

    2020-06-24

--- math/nonnegtive/Algorithm/example_paper/egRand.m
+++ math/nonnegtive/Algorithm/example_paper/egRand.m
@@ -45,24 +45,27 @@ b=2*rand(m,1)-1;
 % [xk1,fk1,xkArr1,countF1,countN1]=hybrid1(x0,A,b);
 % [xk2,fk2,xkArr2,countF2,countN2]=hybrid2(x0,A,b);
 
-i=1
-for m=100:100:500
+
+for m=1000:1000:5000
     for ratio=0.1:0.3:0.8
         n=ceil(ratio*m);
         A=2*rand(m,n)-1;
         b=2*rand(m,1)-1;
         x0=zeros(n,1);
-        
+        t=clock;
         [xk1,fk1,xkArr1,countF1,countN1]=hybrid1(x0,A,b);
+        tf1=etime(clock,t);
 %         [xk2,fk2,xkArr2,countF2,countN2]=hybrid2(x0,A,b);
+t=clock;
         [xk6,fk1,xkArr1,countF6,countN6]=hybrid6(x0,A,b,10);
+        tf2=etime(clock,t);
 %         [xk7,fk1,xkArr1,countF7,countN7]=hybrid7(x0,A,b,10);
         r=b-A*xk1;
         r(r<0)=0;
-        fprintf('$ %d \\times %d $ & %4.2f & %4.2f & %d & %d &',m,n,0.5*(r'*r),norm(A'*r),countF1,countN1);
+        fprintf('$ %d \\times %d $ & %4.2f & %4.2f & %d & %d & %4.2f &',m,n,0.5*(r'*r),norm(A'*r),countF1,countN1,tf1);
         r=b-A*xk6;
         r(r<0)=0;
-        fprintf('%4.2f & %4.2f & %d & %d \\\\\n',0.5*(r'*r),norm(A'*r),countF6,countN6);
+        fprintf('%4.2f & %4.2f & %d & %d & %4.2f\\\\\n',0.5*(r'*r),norm(A'*r),countF6,countN6,tf2);
     end
 end
 % [xk2,fk2,xkArr2,countF2,countN2]=hybrid4(x0,A,b);
