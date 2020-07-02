7de96eff0460d38736adc540782722a06317afb1
home <home@lb.com>
2020-07-02 17:07:31 +0800

    2020-07-02

--- math/nonnegtive/Algorithm/hybrid/hybridMP.m
+++ math/nonnegtive/Algorithm/hybrid/hybridMP.m
@@ -27,40 +27,46 @@ if Ar<delt*rn || rn<delt
 end
 %||A'(r)+||<=delt||(r)+|| ||(r)+||<=de
 while Ar>delt*rn && rn>delt
+    bxn=bx'*bx;
     f1x=(x0-l)./a;
     fxc=find(f1x>fx);
     f1x(fxc)=fx(fxc);
     if bxn<=L*f1x'*fx
-       sqrt()
-       disp(['conject gradient:',num2str(r'*r)]);
-    else
-        countFM=countFM+1;
-        %FM algorithm
-        [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
-        xkArr=[xkArr;[xk',fk,0]];
-        disp(['go to implement space:',num2str(r'*r)]);
-    end
-    k=k+1;
-    bxn=bx'*bx;
-    x0=xk;
-end
-    
-    
-    
-    if uIndex<uIter
-        countFM=countFM+1;
-        %FM algorithm
-        [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
-        xkArr=[xkArr;[xk',fk,0]];
-    else
-        countNW=countNW+1;
+       countNW=countNW+1;
         if countNW ==1
             beginNW=countFM;
         end
         uIndex=0;
-        [xk,rk,fk,f0,lambe]=ssqr(x0,A,b);
+        %[xk,rk,fk,f0,lambe]=ssqr(x0,A,b);
+        z0=A*x0-b;
+        AA=find(z0<ee);
+        FF=setdiff(1:m,AA);
+        AI=A(AA,:);
+        d=-AI\(z0(AA));
+        a1=-min(z./A*d);
+        a2=-min(z(FF)./(A(FF,:)*d));
+        xk=x0+a1*d;
+        zk=b-A*zk;
         xkArr=[xkArr;[xk',fk,1]];
+       disp(['conject gradient:',num2str(rk'*rk)]);
+    else
+        countFM=countFM+1;
+        %FM algorithm
+        [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
+        xkArr=[xkArr;[xk',fk,0]];
+        disp(['go to implement space:',num2str(rk'*rk)]);
     end
+    AA=find(rk<ee);
+    FF=setdiff(1:m,AA);
+    b1=zeros(n,1);
+    f1=zeros(n,1);
+    b2=zeros(m,1);
+    f2=zeros(m,1);
+    f1=
+    b2(AA)=p(AA);
+    f2(FF)=p(FF);
+    b(b>0)=0;
+
     uIndex=uIndex+1;
     Ar=norm(A'*rk);
     rn=norm(rk);
@@ -78,63 +84,12 @@ disp(['%hybrid1 m:',num2str(m),' n:',num2str(n),' AT(b-A*x)+:',num2str(Ar),' fk:
 
 
 
-function xk=MPRGP(A,b,x0,L,a,delta,maxIter)
-k=0;r=A*x0-b;
-tol=1e-15;
-[m,n]=size(r);
-[fx,bx]=fbxf(x0,m,tol,r);
-p=fx;
-bxn=bx'*bx;
-while bxn+fx'*fx>delta
-    f1x=(x0-l)./a;
-    fxc=find(f1x>fx);
-    f1x(fxc)=fx(fxc);
-    if bxn<=L*f1x'*fx
-       sqrt()
-       disp(['conject gradient:',num2str(r'*r)]);
-    else
-       FM() 
-       disp(['go to implement space:',num2str(r'*r)]);
-    end
-    k=k+1;
-    bxn=bx'*bx;
-    x0=xk;
-end
-end
-
-function [amax,xc,res]=computeMaxStep(x0,d)
-grt0=(d>0);
-% if all indredient great 0 then natural sartisfied
-if sum(grt0)<1
-    amax=100;
-    res=0;
-else
-    amax=min(x0(grt0)./d(grt0));
-    res=1;
-end
-xc=x0+amax*d;
-end
-
-function [f,b]=fbxf(x0,m,tol,p)
-AA=find(x0<tol);
-FF=setdiff(1:m,AA);
-b=zeros(m,1);
-f=zeros(m,1);
-b(AA)=p(AA);
-f(FF)=p(FF);
-b(b>0)=0;
-end
-
-
-function f=fxf(x0,m,tol,p)
-FF=find(x0>tol);
-f=zeros(m,1);
-f(FF)=p(FF);
-end
-
-function b=bxf(x0,m,tol,p)
-AA=find(x0<tol);
-b=zeros(m,1);
-b(AA)=p(AA);
-b(b>0)=0;
-end
+% function [f,b]=fbxf(x0,m,tol,p)
+% AA=find(x0<tol);
+% FF=setdiff(1:m,AA);
+% b=zeros(m,1);
+% f=zeros(m,1);
+% b(AA)=p(AA);
+% f(FF)=p(FF);
+% b(b>0)=0;
+% end
