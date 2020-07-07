646334f32b068333a3d701d10b2c35831ebec992
home <home@lb.com>
2020-07-07 16:07:31 +0800

    2020-07-07

--- Untitled.m
+++ Untitled.m
@@ -1,3 +1,28 @@
-syms x y
-%y=((1+0.1)^x)/(1+0.1*x);
-limit(((1+0.1)^x)/(1+0.1*x),x,inf)
+% syms x y
+% %y=((1+0.1)^x)/(1+0.1*x);
+% limit(((1+0.1)^x)/(1+0.1*x),x,inf)
+
+clc
+clear
+addpath('../FM')
+tfA1=[];
+dfA1=[];
+tfA6=[];
+dfA6=[];
+dim={};
+maIter=500;
+nmax=500;
+etc=0.5;
+ete=2;
+rou=0.99;
+trmax=1e12;
+trr=1;
+
+for m=1000:1000:2000
+for ratio=0.1:0.01:0.3
+        n=ceil(ratio*m);
+        A=2*rand(m,n)-1;
+        b=2*rand(m,1)-1;
+        x0=zeros(n,1);
+        t=clock;
+        [xk1,fk1,xkArr1,countF1,countN1]=hybrid1(x0,A,b,maIter);
--- math/nonnegtive/Algorithm/FM/test_FM.m
+++ math/nonnegtive/Algorithm/FM/test_FM.m
@@ -5,18 +5,33 @@
 % AP=pinv(A);
 % [x11,f1]=FM(x0,AP,0,A,b)
 
-A=[1,1;-1,-1;-1,0];
-b=[10,10,50]';
-x0=[-100;-100];
+% A=[1,1;-1,-1;-1,0];
+% b=[10,10,50]';
+% x0=[-100;-100];
+m=100;n=70;
+A=100*rand(m,n)-1;
+b=2*rand(m,1)-1;
+x0=zeros(n,1);
+
 [Q,R]=qr(A);
 all=[];
 x0GS=x0;
-for i=1:10
-[xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
+xk=x0;
+for i=1:100
 
-all=[all [xk;xkGS]];
+
+if i>2
+    all=[all norm(A*(xk-x0))/norm(A*(x0-x1))];
+end
+x1=x0;
 x0=xk;
-x0GS=xkGS;
+    [xk,r0,rk,fk,fm,fr]=FM(x0,Q,R,A,b);
+
+% 
+% all=[all [xk;xkGS]];
+% x0=xk;
+% x0GS=xkGS;
 end
 all
-[xkGS,r0GS,rkGS,fkGS,fmGS,frGS]=FMGS(x0GS,A,b);
+% all
+% [xkGS,r0GS,rkGS,fkGS,fmGS,frGS]=FMGS(x0GS,A,b);
