aafee427093df9ac54f07101f0521e7b4c899ad3
libin <libin@sohu.com>
2020-07-17 15:01:58 +0800

    2020-07-17

--- math/nonnegtive/Algorithm/test_2/Untitled.m
+++ math/nonnegtive/Algorithm/test_2/Untitled.m
@@ -8,7 +8,9 @@ b=[-1;2];
 [Q,R]=qr(A);
 x0=1;
 r=b-A*x0;
-[xk,rk]=FixedM(x0,Q,R,A,b,r)
-[xk,rk]=FixedM(xk,Q,R,A,b,rk)
-[xk,rk]=FixedM(xk,Q,R,A,b,rk)
-[xk,rk]=FixedM(xk,Q,R,A,b,rk)
+[xk1,rk1]=FixedM(x0,Q,R,A,b,r)
+[xk2,rk2]=FixedM(xk1,Q,R,A,b,rk1)
+[xk3,rk3]=FixedM(xk2,Q,R,A,b,rk2)
+[xk4,rk4]=FixedM(xk3,Q,R,A,b,rk3)
+rka=[r,rk1,rk2,rk3,rk4];
+plot(rka(1,:),rka(2,:),'o');
