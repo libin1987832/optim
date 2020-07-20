4d8bbfddee7cfc36204ed0e3b24bb23b49f68df5
libin <libin@sohu.com>
2020-07-20 18:01:39 +0800

    2020-07-20

--- math/nonnegtive/Algorithm/test_2/Untitled2.m
+++ math/nonnegtive/Algorithm/test_2/Untitled2.m
@@ -3,13 +3,21 @@ x = linspace(-5,3);
 m=size(x,2);
 y = linspace(-4,1);
 n=size(y,2);
-[X,Y] = meshgrid(x,y);
+[x,y] = meshgrid(x,y);
 z=zeros(m,n);
+b=[4;5];
+A=[2 2;2 3];
 for i=1:m
     for j=1:n
-    z(i,j) = [4,5]*[x(i,j);y(i,j)]+0.5*[x(i,j),y(i,j)]*[2 2;2 3]*[x(i,j);y(i,j)];
+    z(i,j) = b'*[x(i,j);y(i,j)]+0.5*[x(i,j),y(i,j)]*A*[x(i,j);y(i,j)];
     end
 end
-contour(x,y,z)
-
+contour(x,y,z,[-4.3,-4,-4.4,-4.45,-4.49,-1,-4.1667],'ShowText','on')
+hold on 
+grid on
+x1=[-2;-1/3];
+A1=[2 0;0 15];
+b'*x1+0.5*x1'*A*x1
+b'*x1+0.5*x1'*A1*x1
 %ezcontour(fh,[1,2,3]);
+x=Gauss(A,-b,[0;0],1e-5,2)
