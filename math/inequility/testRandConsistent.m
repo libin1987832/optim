ef0b4edcca2800752f05a14e5d7ccaebcbb1e864
home <home@lb.com>
2020-09-15 07:36:34 +0800

    f

--- math/inequility/testRandConsistent.m
+++ math/inequility/testRandConsistent.m
@@ -1,6 +1,6 @@
 clear;
 clc;
-m = 100;
+m = 1000    ;
 ratio=0.5;
 n = floor( ratio * m);
 A = 2 * rand(m , n)-1;
@@ -11,11 +11,15 @@ nf = 3;
 str = ['D','C','P','R'];
  [xk,rk,countFM,countNW,beginNW,tf,vk,Arr,rkrr]=als(x0,A,b,maxIter);
  Arr(:,end)
-iterA=size(4,maxIter);
+iterA=size(4,maxIter+2);
+maxIterA = 0;
  for i=1:4
     type = str(i);
     [xkD,flag,relres,iter,resvec,itersm,tfD]=hybridA(A,b,x0,maxIter,nf,[type,'HA']);
     iterA(i,1:iter+1)=resvec;
+    if maxIterA < iter+1
+        maxIterA = iter+1;
+    end
     rkD=b-A*xkD;
     rkD(rkD<0)=0;
     dD=norm(rkD);
@@ -25,11 +29,12 @@ iterA=size(4,maxIter);
     fprintf('%s $ %d \\times %d $ & %g & %g & %g & %g & %g & %g\n',type,m,n,dD,gD,tfD,iter*nf,sumiter,beginN);
    % fprintf('$ %d \\times %d $ & %g & %g & %4.4f & %g & %g & %g\n',m,n,dD,gD,tfD,iter*nf,sumiter,beginN);
  end
-type=['r','g','d','k'];
+type=['r','g','k','c'];
 figure
-semilogy(1:maxIter,iterA(1,:),'b.');
+semilogy(1:maxIterA,iterA(1,1:maxIterA),'b.');
 hold on
 for i=2:4
-    semilogy(1:maxIter,iterA(i,:),[type(i) 'o']);
+    semilogy(1:maxIterA,iterA(i,1:maxIterA),[type(i) '.']);
 end
+legend('DHA','CHA','PHA','RHA');
     
