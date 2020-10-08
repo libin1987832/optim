d760c098c7ed9c79229f4b7fb8ca24fccbb9bd6e
home <home@lb.com>
2020-10-08 16:08:30 +0800

    2020-10-08

--- math/inequility/untitled.m
+++ math/inequility/untitled.m
@@ -1,5 +1,29 @@
+addpath('./util')
+gamm1 = 0.5;
+[A1,b1,A2,b2,At1,At2] = readBreast(gamm);
+maxIter = 900;
+str = ['D','C','R','P'];
+x0=zeros(size(A1,2),1);
+[xkh1,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A1,b1,maxIter);
+[xkh2,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A2,b2,maxIter);
+xkh = [xkh1;xkh(1:end-1)];
+gamm = [gamm1,xkh2(end,1)];
+for i = 1:2
+    count1 = sum(At1*xkh(:,i) < gamm(i));
+    count2 = size(At1,1);
+    percent1 = count1/count2;
+    [-count1+count2 percent1]
+    count21 = sum(At2*xkh(:,i) > gamm(i));
+    count22 = size(At2,1);
+    percent2 = count21/count22;
+    [-count21+count22 percent2]
+end
+
+
+
+
 %flops(0)           %start global flop count at 0
-% A=[1 2 3; 4 5 6];  
+% A=[1 2 3; 4 5 6];
 % b=[7 8 9]';
 % x=A*b;             %do the operation
 % addflops(A*b) %do the counting
--- math/inequility/util/readBreast.m
+++ math/inequility/util/readBreast.m
@@ -1,9 +1,23 @@
+function [A1, b1, A2, b2, At1, At2] = readBreast(gamm)
 filename = 'breast-cancer-wisconsin.data';
 format = '%n%n%n%n%n%n%n%n%n%n%n';
-
 [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11] = textread(filename , format , 'delimiter', ',');
-class2 = (data11 == 2);
-class4 = (data11 == 4);
-AB = [data2,data3,data4,data5,data6,data7,data8,data9,data10];
-A = AB(class2);
-B = AB(class4);
+AB = [data2,data3,data4,data5,data6,data7,data8,data9,data10,data11];
+nonnegative = not(sum(AB<0,2));
+AB=AB(nonnegative, : );
+class2 = (AB( : , end ) == 2);
+class4 = (AB( : , end ) == 4);
+A = AB(class2,1:end-1);
+B = AB(class4,1:end-1);
+[m1,n1] = size(A);
+[m2,n2] = size(B);
+rm1 = randperm(m1);
+rm2 = randperm(m2);
+fm1 = floor(m1/3);
+fm2 = floor(m2/3);
+A1 = [-A(rm1(1:fm1),:);B(rm2(1:fm2),:)];
+b1 = [(1 - gamm)*ones(fm1,1);(1+gamm)*ones(fm2,1)];
+A2 = [-A(rm1(1:fm1),:), ones(fm1,1);B(rm2(1:fm2),:), -ones(fm2,1)];
+b2 = ones(fm1+fm2, 1);
+At1 = A(rm1(fm1+1:end),:);
+At2 = B(rm2(fm2+1:end),:);
