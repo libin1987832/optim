edba2bedd50952da8a63d55bda13dd23db85a8aa
home <home@lb.com>
2020-11-01 09:07:30 +0800

    2020-11-01

--- math/inequility/untitled2.m
+++ math/inequility/untitled2.m
@@ -9,14 +9,13 @@ A = 2 * rand(m , n)-1;
     [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
      rkh=b-A*xkh;
      rkh(rkh<0)=0;
-    dh=norm(rkh);
+    dhn=norm(rkh);
      gh=norm(A'*rkh);
-    fprintf('han:%g %4.4f\n',gh,tfh);
-    I = eye(n);
+     I = eye(n);
     xxx = [];
 for i = 1:1000
 u = rand(n,1);
-u = u.* xkh*5;
+u = u.*500;
 dht = b-A*u;
 dh = dht;
 dh2 = -dht;
@@ -28,6 +27,9 @@ d1 = -(u-xkh)'*(A'*A)*h;
 d2 = h'*h;
 d3 = d1-d2;
 d4 = A'*dh2+A'*(dh3);
-xxx = [xxx [d1;d2;d3;-d4'*h;-h'*A'*dh2;-h'*A'*(dh3)]];
+xxx = [xxx [d1;d2;d3;-d4'*h;h'*A'*dh2;h'*A'*(dh3)]];
 end
+xxx
 sum(xxx(1,:) < xxx(2,:))
+  fprintf('han:%g %4.4f %g\n',gh,tfh,dhn);
+     
