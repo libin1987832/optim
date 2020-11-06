b0376372b3d5f893e478b530675051295c39fa74
home <home@lb.com>
2020-11-06 12:07:25 +0800

    2020-11-06

--- Untitledf.m
+++ Untitledf.m
@@ -1,2 +1,16 @@
-AA=[2 3 4;1 2 2;3 3 1];
-LU(AA)
+for i =1 :1000
+A=rand(4,4)*10+123;
+ATA = A'*A;
+l1 = max(eig(ATA));
+B = rand(1,4);
+B = round(B);
+l2 = max(eig(ATA*diag(B)));
+if l1 > l2
+    save('ss','A','B')
+    [l1 l2];
+end
+end
+
+A =  [1 -3;-3 1];
+eig(A'*A*[1 0;0 0])
+[1,5]*A'*A*[1 0;0 0]*[1;5]
