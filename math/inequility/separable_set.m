a69af338929a5d58c55f30f188a19af3120ffe04
home <home@lb.com>
2020-11-18 04:07:46 +0800

    2020-11-18

--- math/inequility/separable_set.m
+++ math/inequility/separable_set.m
@@ -1,13 +1,25 @@
 clear
 clc
-
-n = 500;
+gamm = 0.2;
+n = 10;
 w = [1,1];
 b = 1;
 x = rand( n , 2 );
-y = (x*w'-b*ones(n,1) >0);
-p1 = x(y,:);
-p2 = x(~y,:);
+% index = zeros(n,1);
+% for i = 1:n
+%     xw = x(i,:)*w';
+%     if xw < gamm+1 && xw > gamm -1
+%         index(i) = 1;
+%     end
+% end
+%x = x(~index,:);
+% y = (x*w'-b*ones(size(x,1),1) >0);
+% p1 = x(y,:);
+% p2 = x(~y,:);
+px1 = 0:0.2:1;
+p1 = [px1' -px1'+1.5];
+p2 = [px1' -px1'+0.5];
+plot(p1(:,1),p1(:,2),'r*',p2(:,1),p2(:,2),'+');
 % plot(p1(:,1),p1(:,2),'r*',p2(:,1),p2(:,2),'+');
 
 %[xkh1,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A1,b1,maxIter);
@@ -15,14 +27,14 @@ p2 = x(~y,:);
 
 %source_train = [p1;p2];
 %label_train = [ones(size(p1,1),1);-1*ones(size(p2,1),1)];
-gamm = 0.3;
+
 fm1 = size(p1,1);
 fm2 = size(p2,1);
 A1(1:fm1,:) = p1;
 A1(fm1+1:fm1+fm2,:) = -p2;
 A2 = [[p1 -1*ones(fm1,1)];[-p2 ones(fm2,1)]];
-b1 = [(1 + gamm)*ones(fm1,1);(1 - gamm)*ones(fm2,1)];% 
-b2 = [ones(fm1,1);-ones(fm2,1)];
+b1 = [(1 + gamm)*ones(fm1,1);(gamm-1)*ones(fm2,1)];% 
+b2 = [ones(fm1,1);ones(fm2,1)];
 source_train = [A1(1:fm1,:);-A1(fm1+1:fm1+fm2,:)];
 label_train = [ones(fm1,1);-1*ones(fm2,1)];
 %label_train = round(rand(fm1+fm2,1));
@@ -36,8 +48,8 @@ maxIter = 10;
 norm(rkh)
 [xkh2,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han([x0;0],A2,b2,maxIter);
 norm(rkh)
-ww = [w; wsvn'; xkh1'; ];
-bb = [b; -bsvn; gamm+1 ; ];
+ww = [w; wsvn'; xkh1';xkh1'; xkh2([1,2])' ];
+bb = [b; -bsvn; gamm+1 ;gamm-1;xkh2(3)];
 % ª≠∂‡Ãıœﬂ
 d = lineData(ww , bb, [0,1], [0,1]);
 plot(p1(:,1),p1(:,2),'r*',p2(:,1),p2(:,2),'+');
@@ -45,7 +57,8 @@ hold on
 line(d(:,[1,2])',d(:,[3,4])')
 %line([1,(-bsvn-wsvn(1))/wsvn(2)],[(-bsvn-wsvn(2))/wsvn(1),1])
 %plot(d1)
-[w/norm(w);wsvn'/norm(wsvn);xkh1'/norm(xkh1);xkh2([1,2])'/norm(xkh2([1,2]))]
+[[w b]/norm(w);[wsvn' -bsvn]/norm(wsvn);[xkh1' gamm+1]/norm(xkh1);[xkh2([1,2])' xkh2(3)]/norm(xkh2([1,2]))]
+
 
 
 
