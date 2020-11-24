2c29c4d54186fe3ed899904f7a8c7381ee7a23e6
libin <libin@sohu.com>
2020-11-24 14:01:46 +0800

    2020-11-24

--- math/nonnegtive/Algorithm/MPRGP/LCPSolveL.m
+++ math/nonnegtive/Algorithm/MPRGP/LCPSolveL.m
@@ -13,50 +13,51 @@ if min(q)>=0 % If all elements are positive a trivial solution exists
     z = zeros(size(q));
 else 
     dimen = size(M,1); % Number of rows
-    % Create initial tableau
-    % |   |    |    |   |
-    % | I | M | e | q |
-    tableau = [eye(dimen), M, ones(dimen, 1), q];
+
     
     % Let artificial variable enter the basis
     basis = 1:dimen; % A set of row indices in the tableau
-    
+
     % 选择最小元素，作为输入变量
-    [~,locat] = min(tableau(:,end)); % Row of minimum element in column 
+    [~,locat] = min(q); % Row of minimum element in column 
                                      % 2*dimen+1 (last of tableau)
     Ie=zeros(dimen,1);
     Ie(locat) = -1;
-  %  tableau = [eye(dimen), M, Ie, q];                                 
-    tableauN = [eye(dimen), M, Ie, q];                                   
+   % Create initial tableau
+    % |   |    |    |   |
+    % | I | M | e | q |
+    tableau = [M, Ie, q];
+    P = -ones(dimen,1);
     % 选择基向量的索引                               
     basis(locat) = 2*dimen+1; % Replace that index with the column
     
     % 选择基向量的索引   
-    cand = locat + dimen;
+    cand = locat;
     
-    pivot =  tableauN(locat,:)/tableau(locat,2*dimen+1);
+    pivot =  tableau(locat,:)/P(locat);
      
     % From each column subtract the column 2*dimen+1, multiplied by pivot
     % P = I - pivot
-    tableauN = tableauN - tableau(:,2*dimen+1)*pivot; 
-    tableauN(locat,:) = -pivot; % set all elements of row locat to pivot
+    tableau = tableau - P*pivot; 
+    tableau(locat,:) = pivot;
     % Perform complementary pivoting
     while max(basis) == 2*dimen+1 && loopcount < maxits
         loopcount = loopcount + 1;
-        tableau = tableauN;
         eMs = tableau(:,cand); % This is used to check convergence (pivtol)
         missmask = eMs >= 0;  % Check if elements of eMs are less than zero
-        quots = tableau(:,2*dimen+2)./eMs;
+        quots = tableau(:,dimen+2)./eMs;
         quots(missmask) = -Inf;
         [~,locat] = max(quots);
         % Check if at least one element is not missing
         if  sum(missmask)~=dimen && abs(eMs(locat)) > pivtol 
-            tableauN(:,cand) = zeros(dimen,1); 
-            tableauN(locat,cand) = -1; 
+            P = -tableau(:,cand);
+            tableau(:,cand) = zeros(dimen,1); 
+            tableau(locat,cand) = -1; 
             % Reduce tableau
-            pivot =  tableauN(locat,:)/tableau(locat,cand);
-            tableauN = tableauN - tableau(:,cand)*pivot;
-            tableauN(locat,:) = -pivot;
+            pivot =  tableau(locat,:)/P(locat);
+            tableau = tableau - P*pivot;
+            tableau(locat,:) = pivot;
+            
             oldVar = basis(locat);
             % New variable enters the basis
             basis(locat) = cand;
