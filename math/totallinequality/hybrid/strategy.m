ebdec7d99b02fd2d920c62a85b8e61191fcd3303
libin <libin@sohu.com>
2020-11-26 16:03:08 +0800

    2020-11-26

---
+++ math/totallinequality/hybrid/strategy.m
@@ -0,0 +1,90 @@
+
+function isSub = strategiegy(A,b,x0,steplengthOr,type,iter,nf,rkp,xA)
+[m,n] = size(A);
+isSub = false;
+eIter = 2;
+con1 = 0.95;
+con2 = 0.2;
+diff = 100*eps;
+tol = 1e-13;
+Daxiter = floor(max(33,(m+n)/4)/nf);
+switch upper(type)
+    case 'GHA'
+        rk=rkp;
+        AA=find(rk>1e-15);
+        qrkn=A(AA,:)'*rk(AA);
+        qrkn=A*qrkn;
+        rkn=rkp-eIter*steplengthOr*qrkn;
+        ssign=sum(~xor(rk>tol, rkn>tol));
+        [xk2,~]=krylov(A,b,x0,rkn);
+        constraint = ~all(xk2>-1e15);
+        if ssign==m & constraint
+            isSub = true;
+        end
+    case 'DHA'
+        if mod(iter,Daxiter) == 0
+            isSub = true;
+        end
+    case 'PHA'
+        rk=rkp;
+        xk = xA(:,end);
+        xk1 = xA(:,end - 1);
+        uk =  xk - xk1;
+        rkn=rkp - eIter * A * uk;
+        ssign=sum(~xor(rk>tol, rkn>tol));
+        [xk2,~]=krylov(A,b,x0,rkn);
+        constraint = ~all(xk2>-1e15);
+        if ssign==m & constraint
+            isSub = true;
+        end
+    case 'CHA'
+        x0 = xA(:,end - 1);
+        xpn = xA(:,end - 2);
+        xk = xA(:,end);
+        p1v=x0-xpn;
+        p1=p1v'*p1v;
+        p2v=xk-x0;
+        p2=p2v'*p2v;
+        roup=p2/p1;
+        if roup<con1 && roup>con2
+            isSub = true;
+        end
+     case 'RHA'
+        x0 = xA(:,end-1);
+        xk = xA(:,end);
+        rpk0 = b - A * x0;
+        rpk = b - A * xk;
+        r0 = rpk0;
+        rk = rpk;
+        r0(r0<0) = 0;
+        rk(rk<0) = 0;
+        %|| L(xk,zk) ||
+        L1 = r0'*r0;
+        %||L(xk+1,zk+1)||^2
+        L2 = rk'*rk;
+        z0=-rpk0;
+        z0(z0<0)=0;
+        % -(b-Axk+1)-zk
+        LMv=-rpk-z0;
+        LM=LMv'*LMv;
+        %L(xk,zk)-L(xk+1,zk)
+        L1m=L1-LM;
+        %L(xk+1,zk)-L(xk+1,zk+1)
+        L2m=LM-L2;
+        %[L(xk,zk)-L(xk+1,zk)]-[L(xk+1,zk)-L(xk+1,zk+1)]
+        LL=L1m-L2m;
+        
+        % active set r0
+        AAz=(rkp>tol);
+        %Au=-(b-Ax_{k+1})+(b-Axk)
+        Au=-rpk+rpk0;
+        % Au(AAz)
+        AuAA=Au(AAz);
+        %|| Au(AAz) ||^2
+        LLA=AuAA'*AuAA;
+        if abs(LLA-LL) < diff
+            isSub = true;
+        end
+    otherwise
+        error(message('MATLAB:InvalidOp'))
+end
