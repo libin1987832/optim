69a0ae6786a53910e176ed903f433cd338dca926
home <home@lb.com>
2020-11-29 22:08:09 +0800

    2020-11-29

--- math/totallinequality/testNonnegative.m
+++ math/totallinequality/testNonnegative.m
@@ -5,8 +5,8 @@ addpath('./exact')
 addpath('./hybrid')
 clear 
 clc
-m1 = 500; 
-m2 = 500; n = 700;
+m1 = 100; 
+m2 = 100; n = 70;
 A1=sprand(m1,n,0.1,1/100);
 A2=sprand(m2,n,0.1,1/100);
 % A1=rand(m1,n);
@@ -37,11 +37,12 @@ options.StepTolerance = 1e-13;
 % h.LineStyle = '--';
 % hold on 
  alpha = 1/max(eig(A'*A));
-[xk2,resvec,arvec,face1h,face2h,tf2] = hybridnnls(A,b,x0,alpha,2,maxIterA,options);
+[xk2,resvec,arvec,face1h,face2h,tf2] = hybridnnls(A,b,x0,alpha,5,maxIterA,options);
 [rpk2, normr2, xmin2, Ar, normKKT2 , face12, face22] = kktResidual(A, b, xk2 , [], 1);
- [xk3,resvec3,arvec3,face1vec3,face2vec3,tf3]=fsearchx(A,b,x0,1e-5,1e-8,40);
+ [xk3,resvec3,arvec3,face1vec3,face2vec3,tf3]=fsearchx(-A,-b,x0,1e-13,1e-13,500);
 [rpk3, normr3, xmin3, Ar3, normKKT3 , face13, face23] = kktResidual(A, b, xk3 , [], 1);
-
+[tf2 normKKT2 normr2
+ tf3 normKKT3 normr3]
 % 
 %  fprintf('& %s & %g & %g & %g & %g &%g \\\\\n',['Hybrid'],normr2,xmin2,normKKT2,Ar,tf2);
       
