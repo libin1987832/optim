85928b924159830ef67804032646f57d22b7b209
home <home@lb.com>
2020-12-02 16:07:26 +0800

    2020-12-02

--- math/Discard/untitled.m
+++ math/Discard/untitled.m
@@ -1,7 +1,19 @@
-
-y1 = @(x,k) 4*sin(pi*x*k)/(pi*k);
-x = -1:0.01:1;
-for i = 1:10
-    yy1=y1(x,1);
+clear 
+clc
+y1 = @(x,k) 2*(sin(pi*k)/(pi*k)-cos(pi*k))*sin(pi*x*k)/(pi*k);
+x = -3:0.01:3;
+ss= [];
+for i = 15:20
+    yy1 = zeros(size(x));
+    for j = 1:20
+        if j <i
+            yy1=yy1+y1(x,j);
+        end    
+    end
+    ss(i).Y = yy1;
+    ss(i).X = x;
 end
-array
+figure 
+hold on
+arrayfun(@(a) plot(a.X,a.Y),ss)
+hold off
