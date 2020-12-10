872734414cb8700e24f9794d27555b467451469b
libin <libin@sohu.com>
2020-12-10 10:01:32 +0800

    2020-12-10

--- math/totallinequality/testNonnegative.m
+++ math/totallinequality/testNonnegative.m
@@ -12,7 +12,7 @@ clear
 % 1:fixed 0:lsqin 2:hybrid lsqr 6:hybrd IPG 3: IPG 4: steep decent 5:Newton
 % 7:GNP  8 hybridfast
 example =11;
-[A,b,x0] = readData(4,1000,1000,500);
+[A,b,x0] = readData(1,1000,1000,500);
 [m,n] =size(A);
 options = optimoptions('lsqlin','Algorithm','interior-point','Display','iter');
 options.Display = 'off';
