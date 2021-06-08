8c96460a0c3f9dd7b0b9f5a9f70eb67bf96674f7
gd <gd@gd.com>
2021-06-08 19:59:57 +0800

    wip

--- math/inequality/algorithmInequality/sm.m
+++ math/inequality/algorithmInequality/sm.m
@@ -1,7 +1,7 @@
 % compuation lsqrm: A*x(n)   lsqminnorm: A*x(n) A*xs spiecewise A*x(n)
 function [xs,rpk,len,flag]=sm(A,b,n,rpk,x0,param)
 % lsqr tollera
-tol = param.tol;%1e-15;
+tol = 1e-30;
 lsqrTol = param.lsqrTol;
 maxIter = param.lsqrIter;
 smIsqmIter = param.smIsqmIter;
@@ -12,7 +12,7 @@ AI = A(AA,:);
 bI = rpk(AA);
 
 [u,flag,relres,iter,resvec,lsvec,out] = lsqrm(AI,bI,lsqrTol,maxIter,[],[],zeros(n,1),A,b,x0,AA);
-if ~out
+if 0
     xs = x0 + u;
     len = iter+1.5;
     rpk = b - A * xs;
@@ -29,19 +29,30 @@ else
         I = find(rpk>=tol);
         AI = A(I,:);
   %     hk = lsqminnorm(AI,rpk(I));
-       hk = AI \ rpk(I);
-        if norm(hk) < tol
-            steplength = 0;
-            xs = x0;
-            break
-        end
-%         aa = spiecewise(A,b,hk,x0);
-        steplength = spiecefast(A,b,hk,x0);
+     %  hk = AI \ rpk(I);
+      hk=zeros(n,1);
+ [U,S,V]=svd(AI);
+  [m,n] =size(AI);
+    minmn = min(m,n);
+ svdj=minmn;
+ for j=1:minmn
+     if S(j,j)<1e-20
+         svdj=j;
+         break;
+     end
+ end
+ B=U(:,1:svdj)'*rpk(I);
+ for j=1:svdj
+     B(j)=B(j)/S(j,j);
+ end
+ hk=V(:,1:svdj)*B;
+         steplength = spiecewise(A,b,hk,x0);
+ %       steplength = spiecefast(A,b,hk,x0);
         xs = x0 + steplength * hk;
         x0 = xs;
         rpk = b - A * x0;
    end
-    len = steplength+1e-10;
+    len = steplength;
     flag = flag + 6;
 end
 %rpk = b - A * xs;
