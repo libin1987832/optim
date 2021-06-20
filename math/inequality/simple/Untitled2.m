0887483cfd549491421e5392cf9abc94d33b400b
gd <gd@gd.com>
2021-06-20 17:45:52 +0800

    wip

--- math/inequality/simple/Untitled2.m
+++ math/inequality/simple/Untitled2.m
@@ -6,19 +6,23 @@
 % h3=openfig('HAN.fig','reuse');
 % h4=openfig('DHA.fig','reuse');
 figure
-x=imread('FM.tif');
-subplot(221);
-imshow(x);
-x=imread('IFM.tif');
+x1=imread('FM.tif');
+% subplot(221);
+% imshow(x);
+x2=imread('IFM.tif');
 
-subplot(222);
-imshow(x);
-x=imread('HAN.tif');
-subplot(223);
-imshow(x);
-x=imread('DHA.tif');
-subplot(224);
-imshow(x);
+% subplot(222);
+% imshow(x);
+x3=imread('HAN.tif');
+% subplot(223);
+% imshow(x);
+x4=imread('DHA.tif');
+% subplot(224);
+% imshow(x);
+
+x5 = [x1 x2 ;
+    x4 x4];
+imshow(x5)
 % copyobj(get(get(h1,'Children'),'Children'),s1)
 % s2=subplot(212);
 % copyobj(get(get(h2,'Children'),'Children'),s2)
