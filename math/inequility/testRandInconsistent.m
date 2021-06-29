64707f20e72d6d1eac1efc9fd1e1908d349b8f3e
home <home@lb.com>
2021-06-29 16:07:25 +0800

    2021-06-29

--- math/inequility/testRandInconsistent.m
+++ math/inequility/testRandInconsistent.m
@@ -10,11 +10,12 @@ addpath('./util/');
 %b=rand(rows,1);
 % b=ones(m,1);
 %  b(1:2:end)=-1;
-
+gamm1
 % [A1,b1,A2,b2,At1, At2, fm1, fm2, AL1, bL1, AL2, bL2] = readBreast(gamm1);
  [A1,b1,A2,b2,At1, At2, fm1, fm2, AL1, bL1, AL2, bL2] = readHeart(gamm1); 
-A = A1;
-b = b1;
+% A = A1;
+% b = b1;
+load('Aheart_4.mat');
 [m,n] = size(A);
 % for m = 1000:1000:2000
 %     for ratio = 0.6:0.1:0.8
