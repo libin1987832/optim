05e1cd7a8fbda5c49b4431b35f8449fff2f8bf21
gd <gd@gd.com>
2021-10-22 12:10:26 +0800

    1/10-22-周五

---
+++ math/inequality/randomized algorithm/randomizedGaussSeidelNE.m
@@ -0,0 +1,60 @@
+function [x,iter,error] = randomizedGaussSeidelNE(A, b, x0,maxit,tol,exactx)
+% randomized kaczmarz by Algorithm 1
+% Ax = b
+% A - input matrix
+% b - right vector
+% x0 - initial x
+%
+% x - the approximated x
+% iter - number of iterations before convergence (= maxit if maxit is given)
+% error - the norm of difference in x and exact solution after every
+% iteration
+m = size(A,1);
+n = size(A,2);
+
+x = x0;
+%iter = 0;
+error = [];
+e = 1;
+
+normrow = [];
+index = [];
+%compute norm per row also store the corresponding index
+
+  for i = 1:m
+    normrow = [normrow,norm(A(i,:))];
+    index = [index,i];
+  end
+  
+  weight = normrow/sum(normrow);
+  Acol=sum(A.*A,2);
+if isempty(tol)
+  iter = maxit;   
+  for i = 1:maxit
+    %randsample to generate weighted random number from given vector
+    pickedj = randsample(index,1,true,weight);
+     
+    r=b-A*x;
+    r(r<0)=0;
+     
+    col = A(:, pickedj);
+    x(pickedj) = x(pickedj) + ( col' * r ) / Acol(pickedj);
+    e = norm(x-exactx);
+    error = [error,e];
+    %iter = iter+1;
+  end
+else
+    iter = 1;
+    e = 1;
+    while e >= tol  
+    %randsample to generate weighted random number from given vector
+    pickedi = randsample(index,1,true,weight);
+    
+    row = A(pickedi, :);
+    x = x + ( b(pickedi) - (row * x) ) / (row * row') * row';
+    e = norm(x-exactx);
+    error = [error,e];
+    iter = iter+1;
+    end
+end
+end
---
+++ math/inequality/test_randalg.m
@@ -0,0 +1,44 @@
+%% read data 
+% [A,rows,cols,entries,rep,field,symm]=mmread('../util/well1033.mtx');
+addpath('./dataInequality/');
+addpath('./algorithmInequality/');
+addpath('./randomized algorithm')
+
+m = 1000;
+n = 100;
+rangeMax = 2;
+rangeMin = -2;
+
+   A = 2 * rand(m , n)-1;
+   b = 2 * rand(m , 1)-1;
+   x0 = zeros(n , 1);
+   maxIter = 300;
+   nf = 5;
+   str = ['D','U','C','R','P'];
+% for the solution so here
+
+    [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
+    [rk, rkh, dh, gh] = residual(A,b,xkh);
+    fprintf('active:%d ,%g',vkh,dh);
+    maxit = 10000;
+    [xkacz,iterkacz,errorkacz] = randomizedKaczmarzNE(A, b, x0,maxit,[],xkh);
+    [xGS,iterGS,errorGS] = randomizedGaussSeidelNE(A, b, x0,maxit,[],xkh);
+
+
+
+ %%
+
+beginp = 1;
+figure
+%maxIterA = 200;
+h=semilogy(beginp:iterkacz,errorkacz,'bx');
+h.LineStyle = '--';
+hold on
+h=semilogy(beginp:iterGS,errorGS);
+h.LineStyle = '--';
+legend('Kaczmarz','Gauss Seidel');
+xlabel('the iterative numbers');
+ylabel('the norm of the error');
+
+%    end % for ration
+%    end % for m
