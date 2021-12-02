4a3ff4bef88120080de62eb4a3a10685ebfa3365
gd <gd@gd.com>
2021-12-02 11:10:27 +0800

    1/12-02-周四

--- math/inequality/randomized algorithm/randomized/linear inequalities/gradient_kylov/compareGS_sampleGS.m
+++ math/inequality/randomized algorithm/randomized/linear inequalities/gradient_kylov/compareGS_sampleGS.m
@@ -5,8 +5,42 @@ debug = 1;
 % Ëæ»ú¾ØÕó
 m = 100;
 n = 20;
+r = 50;
+m = 700;
+n = 2*r+1;
 
-A = 2 * rand(m , n)-1;
+%% generate nodes
+%tj are drawing randomly from a uniform distribution in [0,1]
+t = rand(m,1);
+
+%then ordering by magnitude (since they are all positive, just sort)
+t = sort(t);
+% just to assign the size
+w = zeros(m,1); 
+x = zeros(n,1);
+%% generate x 
+realx = randn(n,1);
+imgx = randn(n,1);
+for l = 1:n
+   x(l) = realx(l)+1i*imgx(l);
+end  
+%% generate A and b
+A = zeros(m,n);
+for j = 1:m
+    % dealing with special cases when reach the endpoints(nodes)
+    if j == 1
+        w(j) = (t(2)-t(end)-1)/2;
+    elseif j == m
+            w(j) = (t(1)+1-t(m-1))/2;
+        else
+            w(j) = (t(j+1)-t(j-1))/2;
+    end              
+    for k = -r:r
+       A(j,k+r+1) =sqrt(w(j))*exp(2*pi*1i*k*t(j));
+    end
+end  
+
+% A = 2 * rand(m , n)-1;
 b = 2 * rand(m , 1)-1;
 % b=A*ones(n,1);
 x0 = zeros(n , 1);
