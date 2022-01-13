5eb21e91a50ee9b667a139394786669e38767454
gd <gd@gd.com>
2022-01-13 15:10:27 +0800

    2/01-13-周四

--- math/inequality/randomized algorithm/paper/test/Untitled.m
+++ math/inequality/randomized algorithm/paper/test/Untitled.m
@@ -1,10 +1,15 @@
 A=rand(3,3);
 b=rand(3,1);
 x00=rand(3,1);
-x0=x1;
+x1=x00;
+N=diag([1,0,1]);
 for i=1:3
-    x1=x0+A(:,i)'*(b-A*x0)/(A(:,i)'*A(:,i));
-    x0=x1;
+    x1(i)=x1(i)+A(:,i)'*N*(b-A*x1)/(A(:,i)'*A(:,i));
 end
-ATA=A'*A;
-x01=x
+ATA=A'*N*A;
+D=diag(diag(ATA));
+U=triu(ATA,1);
+L=tril(ATA,-1);
+DL=D+L;
+x01=-inv(DL)*U*x00+inv(DL)*A'*N*b
+x1
