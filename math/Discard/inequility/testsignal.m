a8493333efb6d54690bdf6e10d504b3253ef2564
gd <gd@gd.com>
2022-03-22 16:10:05 +0800

    2/03-22-周二

--- math/Discard/inequility/testsignal.m
+++ math/Discard/inequility/testsignal.m
@@ -1,3 +1,4 @@
+%%%%1
 x=zeros(1,64);
 x(1,6:17) = 0.5:0.5:6;
 x(1,23) = 9;
@@ -7,7 +8,9 @@ x(1,45:55) = ones(1,11)*3.8;
 N = length(x);%求取抽样点数
 t = (0:N-1);%显示实际时间
 hold on
+subplot(411)
 plot(t,x,'g');%绘制时域波形
+title('原始信号')
 gausFilter = fspecial('gaussian',[1 3],3);
 v1 = ones(1,63)*gausFilter(1);
 L1 = diag(v1,1);
@@ -18,13 +21,114 @@ L3 = diag(v3,-1);
 L = L1 + L2 + L3;
 u = rand(1,64)*0.15;
 x2 = L*x'+ u';
+subplot(412)
 plot(t,x2,'r');
-A=[-L;L;eye(64)];
-b=[-x2-0.15;x2-0.15;zeros(64,1)];
+title('接收信号')
+A=[-L;L;eye(64);-eye(64)];
+b=[-x2-0.15;x2-0.15;zeros(64,1);-ones(64,1)*12];
 x0=zeros(64,1);
 maxIter = 30;
-  [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
-  plot(t,xkh','b');
+[xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
+subplot(413)
+plot(t,xkh','b');
+ title('重构信号')
+subplot(414)
+plot(t,x,'g',t,x2,'r',t,xkh,'b')
+legend('原始信号','接收信号','重构信号')
+ title('三种信号对比')
+ 
+ %%%%%2
+%  x=zeros(1,64);
+%  x=0:2*pi/64:2*pi;   %开始：步长：终止数值
+% x=x(1:64);
+% x=(1+1/2*cos(2*pi*1000*x)).*cos(2*pi*10000*x)*2;%绘制函数
+% t=-2*pi/100:pi/1024:2*pi/100;
+% 
+% y=square(2*pi*30*t,50)+abs(min(y));
+% x=[y y(1:23)];
+% N = length(x);%求取抽样点数
+% t = (0:N-1);%显示实际时间
+% hold on
+% subplot(434)
+% plot(t,x,'g');%绘制时域波形
+% gausFilter = fspecial('gaussian',[1 3],3);
+% v1 = ones(1,63)*gausFilter(1);
+% L1 = diag(v1,1);
+% v2 = ones(1,64)*gausFilter(2);
+% L2 = diag(v2);
+% v3 = ones(1,63)*gausFilter(3);
+% L3 = diag(v3,-1);
+% L = L1 + L2 + L3;
+% u = rand(1,64)*0.15;
+% x2 = L*x'+ u';
+% subplot(435)
+% plot(t,x2,'r');
+% A=[-L;L;eye(64);-eye(64)];
+% b=[-x2-0.15;x2-0.15;zeros(64,1);-ones(64,1)*12];
+% x0=zeros(64,1);
+% maxIter = 30;
+% [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
+% subplot(436)
+%  plot(t,xkh','b');
+%  %%%%%3
+%   x=zeros(1,64);
+% x(1,6:17) = 0.5:0.5:6;
+% x(1,23) = 9;
+% x(1,28) = 7;
+% x(1,38) = 2.1;
+% x(1,45:55) = ones(1,11)*3.8;
+% N = length(x);%求取抽样点数
+% t = (0:N-1);%显示实际时间
+% hold on
+% plot(t,x,'g');%绘制时域波形
+% gausFilter = fspecial('gaussian',[1 3],3);
+% v1 = ones(1,63)*gausFilter(1);
+% L1 = diag(v1,1);
+% v2 = ones(1,64)*gausFilter(2);
+% L2 = diag(v2);
+% v3 = ones(1,63)*gausFilter(3);
+% L3 = diag(v3,-1);
+% L = L1 + L2 + L3;
+% u = rand(1,64)*0.15;
+% x2 = L*x'+ u';
+% plot(t,x2,'r');
+% A=[-L;L;eye(64);-eye(64)];
+% b=[-x2-0.15;x2-0.15;zeros(64,1);-ones(64,1)*12];
+% x0=zeros(64,1);
+% maxIter = 30;
+% [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
+%  plot(t,xkh','b');
+%  %%%%%%%4
+%   x=zeros(1,64);
+% x(1,6:17) = 0.5:0.5:6;
+% x(1,23) = 9;
+% x(1,28) = 7;
+% x(1,38) = 2.1;
+% x(1,45:55) = ones(1,11)*3.8;
+% N = length(x);%求取抽样点数
+% t = (0:N-1);%显示实际时间
+% hold on
+% plot(t,x,'g');%绘制时域波形
+% gausFilter = fspecial('gaussian',[1 3],3);
+% v1 = ones(1,63)*gausFilter(1);
+% L1 = diag(v1,1);
+% v2 = ones(1,64)*gausFilter(2);
+% L2 = diag(v2);
+% v3 = ones(1,63)*gausFilter(3);
+% L3 = diag(v3,-1);
+% L = L1 + L2 + L3;
+% u = rand(1,64)*0.15;
+% x2 = L*x'+ u';
+% plot(t,x2,'r');
+% A=[-L;L;eye(64);-eye(64)];
+% b=[-x2-0.15;x2-0.15;zeros(64,1);-ones(64,1)*12];
+% x0=zeros(64,1);
+% maxIter = 30;
+% [xkh,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=han(x0,A,b,maxIter);
+%  plot(t,xkh','b');
+%  
+ 
+ 
 % xff = fft(x);
 % stem(1:64, xff);  
   
