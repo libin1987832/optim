264f72207555ff61219fea61dd2939d627fe155a
home <home@lb.com>
2022-03-27 16:07:28 +0800

    2022-03-27

--- math/inequality/randomized algorithm/paper/app3/CompareSix.m
+++ math/inequality/randomized algorithm/paper/app3/CompareSix.m
@@ -1,18 +1,19 @@
-m1=60;
-m2=30;
-n=100;
-a=0.3;
+m1=600;
+m2=450;
+n=80;
+al=0.2;
+be=0.3;
 A1=rand(m1,n);
 A2=rand(m2,n);
-x=rand(n,0);
+x=rand(n,1);
 d1=A1*x;
 d2=A2*x;
-dn=floor(m1*a);
+dn=floor(m1*al);
 u=d1;
-u(1:dn,:)=u(1:dn,:)+rand(dn,1);
-
+u(1:dn,:)=u(1:dn,:)+be*rand(dn,1).*u(1:dn,:);
+l=d2;
 H=[-A1;A2;eye(n)];
-b=[-d1;u;zeros(n,1)];
+b=[-u;d2;zeros(n,1)];
 x0=zeros(n,1);
 
 maxIter=20;
@@ -20,29 +21,31 @@ iter = 800;
 nf = 20;
 xst=zeros(1,4);
 t=clock;
-[x,alpha,beta,flag]=ablp(A1,A2,u,d2,0.5,0.5,0.1,0.1);
+[x1,alpha,beta,flag]=ablp(A1,A2,u,d2,0.5,0.5,0.1,0.1);
 xst(1)=etime(clock,t);
 t=clock;
 [x2,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=hybridA(H,b,x0,maxIter,nf,'RHA');
 xst(2)=etime(clock,t);
 t=clock;
-[x3,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=hybridA(A,b,x0,maxIter,nf,'PHA');
+[x3,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=hybridA(H,b,x0,maxIter,nf,'PHA');
 xst(3)=etime(clock,t);
 t=clock;
-[x4,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=hybridA(A,b,x0,maxIter,nf,'CHA');
+[x4,rkh,countFMh,countNWh,beginNWh,tfh,vkh,rkArrh]=hybridA(H,b,x0,maxIter,nf,'CHA');
 xst(4)=etime(clock,t);
 
 xs=[x1 x2 x3 x4];
-
+[alpha,beta]
 str=['P','R','P','C'];
 for i = 1:4
-r = -l + A1 * xs(:,i);
+r = -u + A1 * xs(:,i);
 rnum1=sum(r>0);
-r = u - A2 * xs(:,i);
-rnum2=sum(r);
+r = l - A2 * xs(:,i);
+rnum2=sum(r>0);
 r=b-H*xs(:,i);
+r(r<0)=0;
 r_GS = norm(r);
-fprintf('& %sHA &%d &%d & %g & %g \\\\\n', str(i), rnum1,rnum2,r_GS, xst(i));
+rum3=sum(x<0);
+fprintf('& %sHA &%d &%d &%d & %g & %g \\\\\n', str(i), rnum1,rnum2,rum3,r_GS, xst(i));
 end
 
 
--- math/inequality/randomized algorithm/paper/app3/ablp.m
+++ math/inequality/randomized algorithm/paper/app3/ablp.m
@@ -1,4 +1,4 @@
-function [x,alpha,beta,flag]=ablp(A1,A2,l,u,amax,bmax,da,db)
+function [x,alpha,beta,flag]=ablp(A1,A2,u,l,amax,bmax,da,db)
 [m1,n]=size(A1);
 [m2,n]=size(A2);
 c=zeros(m1+n,1);
@@ -7,12 +7,13 @@ alpha0=0;
 beta0=0;
 alpha=alpha0;
 beta=beta0;
-A=[A1,-diag(u);-A2,zeros(m1,m1);zeros(1,n) c'];
+A=[A1,-diag(u);-A2,zeros(m2,m1);c'];
 while 1
 b=[zeros(m1,1);-l;m1*(1+alpha*beta)];
 lb=zeros(m1+n,1);
-ub=[Inf*ones(n,1);(1+beta)*ones(m1,0)];
+ub=[Inf*ones(n,1);(1+beta)*ones(m1,1)];
 [x,fval,flag] = linprog(c,A,b,[],[],lb,ub);
+x=x(1:n,1);
 if flag==1
     break;
 end
