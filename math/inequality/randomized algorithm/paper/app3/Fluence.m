299ad475cc8935e517e0f5ff6d3d44b57f0bf7ca
home <home@lb.com>
2022-03-27 19:07:28 +0800

    2022-03-27

--- math/inequality/randomized algorithm/paper/app3/Fluence.m
+++ math/inequality/randomized algorithm/paper/app3/Fluence.m
@@ -1,8 +1,14 @@
-        
-f = -A'*d;
+function [x]=Fluence(A,B,l,u,k,r)
+[m1,n]=size(A);
+[m2,n]=size(B);
+H=[1/sqrt(m2)*B;sqrt(r/m1)*A];
+w=0;
+dw=[1/sqrt(m2)*l;sqrt(r/m1)*(u+w)];
+              f = -H'*dw;
+              HTH=H'*H;
             if strcmp(prob.nnls,'quadprog')   
                 options = optimoptions(@quadprog,'Display','off');
-                x = quadprog(H,f,[],[],[],[],lb,ub,[],options);
+                x = quadprog(HTH,f,[],[],[],[],lb,ub,[],options);
             else
                 func = @(x)FluenceMapOpt.quadObj(x,H,f);
                 options.verbose = 0;
@@ -19,10 +25,10 @@ f = -A'*d;
             % Grab variables
             
            
-            step = prob.structs{ii}.terms{jj}.step;
-            nVoxels = prob.structs{ii}.nVoxels;
-            coeff = step*weight/nVoxels;
-            
+%             step = prob.structs{ii}.terms{jj}.step;
+%             nVoxels = prob.structs{ii}.nVoxels;
+%             coeff = step*weight/nVoxels;
+            coeff = 1;
             % Calculate gradient step
 %             dose = prob.structs{ii}.A*prob.x;
             %res = (-1)^s*(H*x - d);
@@ -32,7 +38,7 @@ f = -A'*d;
             
             % Project onto set ||(w)_+||_0 <= k
             wProj = projW(wStep,k);
-            wDiff = norm(wProj - wPrev)/step;
+%             wDiff = norm(wProj - wPrev)/step;
             w = wProj;
         end
          function w = projW(w,k)
