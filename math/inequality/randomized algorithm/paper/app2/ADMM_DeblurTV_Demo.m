482de45fd2c57b321ec3a619ff25c405cc676171
home <home@lb.com>
2022-03-29 17:07:28 +0800

    2022-03-29

--- math/inequality/randomized algorithm/paper/app2/ADMM_DeblurTV_Demo.m
+++ math/inequality/randomized algorithm/paper/app2/ADMM_DeblurTV_Demo.m
@@ -8,53 +8,74 @@ clear all;
 
 ImgA={'Lena','male','mandril'};
 name=['1','2','3'];
-load('dd')
-for i=1:3
+
+for i=1:1
 
 set(gcf,'position',[100,100, 700, 600]);
-set(gca,'DataAspectRatio',[1,1,1]); 
-    % Img = imread([name(i) '.tiff']);
-% 
-% if size(Img,3) > 1
-%     Img = rgb2gray(Img);
-% end
-% % ImgA{i}=Img;
-% 
-% K     =   fspecial('average',3); % For denoising
-% f = imfilter(Img,K,'circular');
-% 
-% f = double(f);
-% 
-% BSNR = 20;
-% sigma = BSNR2WGNsigma(f, BSNR);
-% 
-% f = f +  sigma * rand(size(Img)); %Add a little noise
-% 
-% %*** ADMM algorithm parameter set up ***
-% 
-% opts.lam = 1.5;
-% opts.rho = 1.3;
-% opts.tol = 1e-5;
-% opts.Nit = 30;
-% opts.Nosnr=0;
-% out1 = FM_DeblurTV(f,Img,K,sigma,opts);
-% out2 = ADMM_DeblurTV(f,Img,K,opts);
+
+    Img = imread([name(i) '.tiff']);
+
+if size(Img,3) > 1
+    Img = rgb2gray(Img);
+end
+% ImgA{i}=Img;
+
+K     =   fspecial('average',3); % For denoising
+f = imfilter(Img,K,'circular');
+
+f = double(f);
+
+BSNR = 30;
+sigma = BSNR2WGNsigma(f, BSNR)*20;
+
+f = f +  sigma * rand(size(Img)); %Add a little noise
+
+%*** ADMM algorithm parameter set up ***
+
+opts.lam = 1.5;
+opts.rho = 1.3;
+opts.tol = 1e-5;
+opts.Nit = 100;
+opts.Nosnr=0;
+out1 = FM_DeblurTV(f,Img,K,sigma,opts);
+out2 = ADMM_DeblurTV(f,Img,K,opts);
 %subplot(4,3,i)
-sigma=1;
+
 x=(i-1)*0.2;
-subplot('position',[0+x,0,.2,.2]);
+subplot('position',[0+x,0.75,.2,.2]);
+% subfig=subplot(4,3,i);
+% RemoveFigMarginArea(subfig)
+% RemoveSubplotMarginArea(subfig, 4,3,i)
 imshow(Img)
+% set(gca,'DataAspectRatio',[3,4 ,1]);  
 title(sprintf('无噪声干扰的%s',ImgA{i}))
-% subplot(4,3,i+3)
-subplot('position',[0+x,0.25,.2,.2]);
+%subfig=subplot(4,3,i+3)
+%RemoveFigMarginArea(subfig)
+% RemoveSubplotMarginArea(subfig, 4,3,i+3)
+subplot('position',[0+x,0.5,.2,.2]);
+% set(gca,'DataAspectRatio',[3,4,1]);  
 imshow(uint8(f))
 title(sprintf('添加噪声\\sigma=%g',sigma),'Interpreter','tex')
-% % subplot(4,3,i+3)
-subplot('position',[0+x,0.5,.2,.2]);
+% subfig=subplot(4,3,i+6)
+% RemoveFigMarginArea(subfig)
+%  RemoveSubplotMarginArea(subfig, 4,3,i+6)
+subplot('position',[0+x,0.25,.2,.2]);
+% set(gca,'DataAspectRatio',[3,4,1]);  
 imshow(uint8(out2.sol))
-title('全变分的图像重构')
-% % subplot(4,3,i+3)
-subplot('position',[0+x,0.75,.2,.2]);
+title('全变分图像重构')
+% subfig=subplot(4,3,i+9)
+%  RemoveFigMarginArea(subfig)
+% set(subfig,'LooseInset',get(subfig,'TightInset'))
+% RemoveSubplotMarginArea(subfig, 4,3,i+9)
+subplot('position',[0+x,0,.2,.2]);
+% set(gca,'DataAspectRatio',[3,4,1]);  
 imshow(uint8(out1.sol))
-title('全变分的图像重构')
+title('线性方程组图像重构')
+
 end                   % plot(out.relativeError)
+
+fdata = getframe(gcf);
+figure
+imshow(fdata.cdata(:,10:420,:))
+%imwrite(f.cdata(:,10:420,:), 'imagediff.fig');
+%imwrite(f.cdata(:,10:420,:), 'imagediff.eps');
--- math/inequality/randomized algorithm/paper/app2/plotsnrpsnr.m
+++ math/inequality/randomized algorithm/paper/app2/plotsnrpsnr.m
@@ -7,7 +7,7 @@ clc;
 clear all;
 close all;
 
-Img = imread('Lena_std.tif');
+Img = imread('1.tiff');
 
 if size(Img,3) > 1
     Img = rgb2gray(Img);
@@ -19,7 +19,7 @@ f = imfilter(Img,K,'circular');
 f = double(f);
 
 BSNR = 20;
-sigma = BSNR2WGNsigma(f, BSNR)
+sigma = BSNR2WGNsigma(f, BSNR)*20;
 f = f +  sigma * rand(size(Img));
 % u=Img;
 % relKu=imfilter(u,K,'circular');
@@ -41,8 +41,8 @@ f = f +  sigma * rand(size(Img));
 opts.lam = 1.5;
 opts.rho = 1.3;
 opts.tol = 1e-5;
-opts.Nit = 30;
-
+opts.Nit = 10;
+opts.Nosnr = 1;
 out1 = FM_DeblurTV(f,Img,K,sigma,opts);
 out2 = ADMM_DeblurTV(f,Img,K,opts);
 figure;
@@ -59,6 +59,8 @@ ylabel('结构相似性(SSIM)')
 legend('最小二乘意义下线性不等式方程组','全变分模型')
 figure
 imshow(uint8(out1.sol))
+figure
+imshow(uint8(out2.sol))
 psnr_fun=psnr(out1.sol,double(Img));
 ssim_index=ssim(out1.sol,double(Img));
 
