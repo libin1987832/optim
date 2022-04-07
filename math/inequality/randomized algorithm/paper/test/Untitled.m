17ac23c94181438740b47706431a798b79e4c9d2
gd <gd@gd.com>
2022-04-07 17:10:27 +0800

    2/04-07-周四

--- math/inequality/randomized algorithm/paper/test/Untitled.m
+++ math/inequality/randomized algorithm/paper/test/Untitled.m
@@ -1,8 +1,8 @@
-A=rand(3,3);
-b=rand(3,1);
+A=rand(5,3);
+b=rand(5,1);
 x00=rand(3,1);
 x1=x00;
-N=diag([1,0,1]);
+N=diag([1,0,1,0,1]);
 lambda = 1.1;
 for i=1:3
     x1(i)=x1(i)+lambda*A(:,i)'*N*(b-A*x1)/(A(:,i)'*A(:,i));
@@ -16,12 +16,31 @@ L=tril(ATA,-1);
 DL=D1+lambda*L;
 y00=x00;
 x01=-inv(DL)*(lambda*U+lambda*D2-D1)*x00+lambda*inv(DL)*A'*N*b;
-x01
-x1
+x02=-inv(DL)*(lambda*U+lambda*D2-D1)*x01+lambda*inv(DL)*A'*N*b;
 r01=b-A*x01;
 r00=b-A*x00;
-2*lambda*A'*N*r01
-lambda*A'*N*r00-(D1+lambda*L)*(x00-x1)
+r02=b-A*x02;
+ lambda*A'*N*r00
+ [U,S,V]=svd((N*A)');
+ (N*r00)'*V(1,:)'
+  (N*r01)'*V(1,:)'
+    (N*r02)'*V(1,:)'
+%  (D1+lambda*L)*(x00-x1)
+% [U,S,V]=svd(A);
+% U=U(:,1:3);
+% S=S(1:3,1:3);
+% lambda*V*S*U'*N*r00
+% (D1+lambda*L)*V*diag([1/S(1,1),1/S(2,2),1/S(3,3)])*U'*(r01-r00)
+
+% x01
+% x1
+% r01=b-A*x01;
+% r00=b-A*x00;
+% 2*lambda*A'*N*r01
+% lambda*A'*N*r00-(D1+lambda*L)*(x00-x1)
+
+
+
 % for i=1:200
 % y01=-inv(DL)*(lambda*U+lambda*D2-D1)*y00+lambda*inv(DL)*A'*N*b;
 % y00=y01;
