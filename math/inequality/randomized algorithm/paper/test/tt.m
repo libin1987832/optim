dd1f54e51e0d9d609275e5286a3c8ea20e104e99
home <home@lb.com>
2022-04-10 09:07:20 +0800

    2022-04-10

--- math/inequality/randomized algorithm/paper/test/tt.m
+++ math/inequality/randomized algorithm/paper/test/tt.m
@@ -1,10 +1,13 @@
 % A=diag(floor(rand(5,1)*10)) + tril(floor(rand(5,5)),-1)
 % inv(A)
-m=10;
-n=3;
-A=rand(m,n)*100;
-Aci=diag(1./sqrt(sum(A.*A,1)));
-minAc=min(sum(A.*A,1));
-B=A*Aci;
-min(eig(A'*A))
-minAc*min(eig(B'*B))
+% m=10;
+% n=3;
+% A=rand(m,n)*100;
+% Aci=diag(1./sqrt(sum(A.*A,1)));
+% minAc=min(sum(A.*A,1));
+% B=A*Aci;
+% min(eig(A'*A))
+% minAc*min(eig(B'*B))
+n=100
+(1-1/n)^n
+exp(-1)
