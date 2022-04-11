025244418a15cbb2136faa942a5690157a59e001
gd <gd@gd.com>
2022-04-11 16:10:31 +0800

    2/04-11-周一

--- math/inequality/randomized algorithm/paper/app1/CompareSix2.m
+++ math/inequality/randomized algorithm/paper/app1/CompareSix2.m
@@ -1,11 +1,11 @@
  [A1,b1,A2,b2,At1, At2, fm1, fm2, AL1, bL1, AL2, bL2] = readBreast(gamm1);
-% [A1,b1,A2,b2,At1, At2, fm1, fm2, AL1, bL1, AL2, bL2] = readHeart(gamm1);
+ %[A1,b1,A2,b2,At1, At2, fm1, fm2, AL1, bL1, AL2, bL2] = readHeart(gamm1);
 A=A1;
 b=b1;
 [m,n]=size(A);
 x0=zeros(n,1);
 
-maxIter=6000;
+maxIter=10000;
 iter = 30;
 nf = 10;
 tol = -1;
