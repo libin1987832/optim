694bea0324f4fdbd945821ba2fc3bb2dacb5e049
gd <gd@gd.com>
2022-04-20 15:10:35 +0800

    2/04-20-周三

--- math/inequality/randomized algorithm/paper/new/ComparePC.m
+++ math/inequality/randomized algorithm/paper/new/ComparePC.m
@@ -4,9 +4,44 @@ debug = 1;
 %% 产生问题矩阵
 m = 1000;
 n = 200;
-A = 2 * rand(m , n)-1;
-b = 2 * rand(m , 1)-1;
+% A = 2 * rand(m , n)-1;
 
+r = 100;
+m = 6000;
+n = 2*r+1;
+% generate nodes
+%tj are drawing randomly from a uniform distribution in [0,1]
+t = rand(m,1);
+
+%then ordering by magnitude (since they are all positive, just sort)
+t = sort(t);
+%just to assign the size
+w = zeros(m,1); 
+x = zeros(n,1);
+% generate x 
+realx = randn(n,1);
+imgx = randn(n,1);
+for l = 1:n
+   x(l) = realx(l)+1i*imgx(l);
+end  
+% generate A and b
+A = zeros(m,n);
+for j = 1:m
+%    dealing with special cases when reach the endpoints(nodes)
+    if j == 1
+        w(j) = (t(2)-t(end)-1)/2;
+    elseif j == m
+            w(j) = (t(1)+1-t(m-1))/2;
+        else
+            w(j) = (t(j+1)-t(j-1))/2;
+    end              
+    for k = -r:r
+       A(j,k+r+1) =sqrt(w(j))*exp(2*pi*1i*k*t(j));
+    end
+end  
+
+A=real(A);
+b = 2 * rand(m , 1)-1;
 x0 = zeros(n , 1);
 tol=1e-10;
 fprintf('%s & %d & %d tol=%g\n','矩阵维数', m, n, tol);
@@ -25,7 +60,7 @@ fprintf('%s & %g & %g \n','最开始的目标函数和梯度', norm_r0, norm_g0);
 % fprintf('%s & %g & %g \n','IFM解的目标函数值和梯度  ', norm_rexact, norm_gexact);
  x_exact=[];
 %% 参数的设定
-maxit_PC = 100000;
+maxit_PC = 1000;
 z0=zeros(m,1);
 t=clock;
 [x_PC,z_PC,iter_PC,error_PC,index_PC]=PC(x0,z0,A,b,maxit_PC,tol,x_exact,debug);
@@ -72,7 +107,7 @@ fprintf('& %s & %g & %g & %d & %g \\\\\n', 'RCD', r_GS, g_GS, iter_GS, tf_GS);
 
 % 
 figure (1)
-iters=10;
+iters=0;
 semilogy(1:iters,error_PC(1:iters),'k--');
 title('比较坐标下降算法和投影收缩算法的性能差异')
 ylabel('梯度的范数') 
