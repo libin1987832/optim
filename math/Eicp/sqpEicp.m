3bf4927d70d839a4038b1caf2913208d24575a52
home <home@lb.com>
2022-06-17 06:07:27 +0800

    2022-06-17

--- math/Eicp/sqpEicp.m
+++ math/Eicp/sqpEicp.m
@@ -1,24 +1,35 @@
-function [x, error] = sqpEicp(A, B, M, x0, method, eps, maxIT, debug)
+function [x, error] = sqpEicp(A, B, M, x0, sigma0, eps, bisectEps, maxIT, debug)
 error = 0;
 x = x0;
 if debug
     error = zeros(1, maxIT);
 end
 k = 0;
-D = diag(M);
+method = 1;
+if norm(diag(D) - M) == 0
+    method = 2;
+end
+maxe = sigma0;
 while 1
     xB = x' * B;   
     Ax = A * x;
     tao = 1 - 0.5 * xB * x;  
-    d = searchdir(Ax, xB, tao, D, x, method);
+    % compute the dirction
+    d = searchdir(Ax, xB, tao, x, method, bisectEps);
     if norm( d ) < eps || k > maxIT
         break;
     end
     Ad = A * d;
+    dAd = d' * Ad;
     Bd = B * d;
     c = 0.5 * d' * Bd;
-    
-    sigma = 
+    e=abs(lambda)+0.01;
+    if e > maxe
+        sigma = e;
+    end
+    z= - ( Ax' * d + tao * sigma ) / ( dAd + 2 * c * sigma );
+    u= ( - tao + sqrt( tao^2 + 4 * c * tao ) ) / ( 2 * c );
+    % compute steplength
     alpha = searchstep(tao, z, u);
     x = x + alpha * d;
     k = k + 1;
