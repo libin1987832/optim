b0a2469bca975aa1abfe1f10b56120fe7286ff4f
gd <gd@gd.com>
2022-06-16 15:10:22 +0800

    2/06-16-周四

--- math/Eicp/searchdir.m
+++ math/Eicp/searchdir.m
@@ -1,14 +1,8 @@
-function [d, lambda] = searchdir(M, Ax, xAx, xB, xBx, tao, D, x, method)
+function [d, lambda] = searchdir(M, Ax, xB, tao, D, x, method)
     if method == 1
         %tao的值
       %%  求dk,直接调用函数 quadprog，(A对称正定时取，M=A.A对称时取M=A+ R*I)
        [d,fval,exitflag,output,lambda]=quadprog(M, Ax, [], [], xB, tao, -x, []);
-       Md = M * d
-       xMd = x' * Md;
-       dMd = d * Md;
-       dAx = d' * Ax;
-       dBx = d' * xB';
-       lambda = ( xMd + xAx + dMd + dAx )/( xBx + dBx);
     elseif method == 2
          Bx = xB';
          Dx = D .* x;
