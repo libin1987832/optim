c204cbb77a36502d67659dde96165f5a4c06671d
gd <gd@gd.com>
2022-06-20 10:10:55 +0800

    2/06-20-周一

--- math/Eicp/BBP.m
+++ math/Eicp/BBP.m
@@ -2,7 +2,7 @@
 %求解  min f(x)=1/2x'*A*x+yk'*x
 %      st. e'*x=1
 %          x>=0
-function [xk]=BBP(A,yk,e,n,epsilon2) % epsilon2表示误差，e表示分量全为1的横向量，e0表示表示分量全为0的横向量，options为了不输出 quadprog中间的计算过程
+function [x, F]=BBP(A, b, F, maxIt, eps) % epsilon2表示误差，e表示分量全为1的横向量，e0表示表示分量全为0的横向量，options为了不输出 quadprog中间的计算过程
 i=0;  %迭代次数
 M=[A -e';e 0]; % M矩阵
 Z=zeros(n+1,1);
