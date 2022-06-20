07232ce4f39e8e4d690ffe0d389d050aab7dcbc0
gd <gd@gd.com>
2022-06-20 14:10:57 +0800

    2/06-20-周一

--- math/Eicp/BBP.m
+++ math/Eicp/BBP.m
@@ -2,38 +2,39 @@
 %求解  min f(x)=1/2x'*A*x+yk'*x
 %      st. e'*x=1
 %          x>=0
-function [x, F]=BBP(A, b, F, maxIt, eps) % epsilon2表示误差，e表示分量全为1的横向量，e0表示表示分量全为0的横向量，options为了不输出 quadprog中间的计算过程
-i=0;  %迭代次数
-M=[A -e';e 0]; % M矩阵
-Z=zeros(n+1,1);
-h=[yk;-1];
-N=1:(n+1);
+function [x, F, iter]=BBP(A, b, F, maxIt, eps, strategy, debug) % epsilon2表示误差，e表示分量全为1的横向量，e0表示表示分量全为0的横向量，options为了不输出 quadprog中间的计算过程
+iter = 0;  %迭代次数
+[ m, n ] = size(A);
+M = [ A -ones( m , 1 ); ones( 1 , m ) 0 ]; % M矩阵
+h = [ b ; -1 ];
+N = 1 : n;
 % N=N';
-F=N;
-v=F;
+F = N;
+
 while 1
- T=setdiff(N, F);
- MFF=M(F,F);
- MTF=M(T,F);
- hF=h(F);
- hT=h(T);
-ZF=MFF\(-hF);
-VT=hT+MTF*ZF;
-ZF1=ZF(1:end-1);
-if ZF1>=-epsilon2
-    if VT>=-epsilon2
-  Z(F)=ZF;
-  Z(T)=0;    
-  xk=Z(1:n); %输出x
-  break
+    T = setdiff( N , F );
+    Fn = union( F,  n + 1  );
+    MFF = M( Fn , Fn );
+    MTF = M( T , Fn );
+    hF = h( Fn );
+    hT = h( T );
+    z = zeros( n + 1 , 1 );
+    v = zeros( n + 1 , 1 );
+    z( Fn ) = - MFF \ hF;
+    v( T ) = hT + MTF * zF;
+    x = z( 1 : n , 1 );
+    if iter > maxIt
+        break;
     end
+    if sum( z( F ) >= -eps ) == 0 && sum( v( T ) >= -eps) == 0 
+        break
+    elseif strategy == 1
+        
+    else
+        F1 = setdiff( F , z < -eps );
+        F2 = v( T ) <= eps;
+        F = union(F1, F2);
+    end
+    iter = iter + 1;
 end
-w1=find(ZF1<-epsilon2);
-v(F)=1;
-v(T)=VT;
-w2=find(v<epsilon2);
-F(w1)=[];%更新指标F
-F=union(F,w2);
-i=i+1;  
-end   
- end 
+end
