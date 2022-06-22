ff8fc80e0331f380f184a14cf8c370a80822335f
gd <gd@gd.com>
2022-06-22 16:10:24 +0800

    2/06-22-周三

--- math/Eicp/qba.m
+++ math/Eicp/qba.m
@@ -1,19 +1,31 @@
-function x = qba(a, B, d, epssub)
+function [x, iter] = qba(x0, B, d, eps, epssub)
 [m, n] = size(d);
 p = 100;
+Ax0 = A * x0;
+x0Ax0 = x0 * Ax0;
+a = d' * x0 / x0Ax0 ;
 if a == 0
-    [dm, imax] = max(d);
-    et = zeros(m, 1);
-    et(imax) = 1;
-else
+    [~, imax] = max(d);
+    x = zeros(m, 1);
+    x(imax) = 1;
+end
+while 1
     F = 1 : n;
     ninf0 = 2 * n;
     while ninf0 >0
         Fold = F;
-        [x1, F, iter, ninf, ~] = BBP(A, - d / a, F, p, ninf0, epssub, 0, debug);
+        [x1, F, iter, ninf, ~] = BBP(B, - d / a, F, p, ninf0, epssub, 0, debug);
         if iter == p
-            [x1, F, iter, ninf, ~] = BBP(A, - d / a, Fold, p, ninf0, epssub, 1, debug);
+            [x1, F, iter, ninf, ~] = BBP(B, - d / a, Fold, p, ninf0, epssub, 1, debug);
         end
         ninf0 = ninf;
     end
-    a = d' * x1/
+    Bx = B * x1;
+    xBx = x * Bx;
+    a1 = (d' * x1) / xBx;
+    if abs( a - a1 ) >= eps
+        x = x1;
+    else
+        a = a1;
+    end
+end
