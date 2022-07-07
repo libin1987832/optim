15c9accf78ec98d2d43b3f0ed4ee1dbd38ca6f64
home <home@lb.com>
2022-07-08 06:07:33 +0800

    2022-07-08

--- math/Eicp/Ax1.m
+++ math/Eicp/Ax1.m
@@ -56,9 +56,10 @@ epsxlambda = -1e-3;
 %tic;[x,  iter, error] = sqpEicp(A, B, M, x1, sigma0, 0.1, 1e-5, 1e-12, 10000, 0);toc
 %lambda = (x' * A * x) / (x' * B * x);
 %disp(['sqp:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',ninfy='  num2str(sum((A - lambda * B) * x < epsxlambda)) ',iter=' num2str(iter)])
-tic;[x, iter, error]=SPL(B, A, x1, 10000,  1e-20, 1e-10, 0);toc
+tic;[x, iter, error]=SPL(B, A, x1, 10000,  1e-6, 1e-6, 0);toc
 lambda = (x' * A * x) / (x' * B * x);
-disp(['spl:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',ninfy='  num2str(sum((A - lambda * B) * x < epsxlambda)) ',iter=' num2str(iter)])
+w = A * x - lambda * B * x; 
+disp(['spl:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',dualfeasible='  num2str(min(w)) ',iter=' num2str(iter)])
 tic;[x, iter, fun] = spBas(A, B, x1, 1e-8, unifrnd (0,1), 1e-8, 1000, 0);toc
 lambda = (x' * A * x) / (x' * B * x);
 disp(['BAS:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',ninfy='  num2str(sum((A - lambda * B) * x < epsxlambda)) ',iter=' num2str(iter)])
