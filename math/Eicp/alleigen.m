5373e505af98b0b4ffd2d64de5977bb2f858382e
home <home@lb.com>
2022-07-09 07:07:27 +0800

    2022-07-09

--- math/Eicp/alleigen.m
+++ math/Eicp/alleigen.m
@@ -5,9 +5,10 @@
 % iter = 1;
 clc
 clear
-A=[5 7 6 5;7 10 8 7;6 8 10 9;5 7 9 10];
+%A=[5 7 6 5;7 10 8 7;6 8 10 9;5 7 9 10];
+A = [0 1 0 1 0 0;1 0 0 1 0 0;0 0 0 1 0 0;1 1 1 0 1 1;0 0 0 1 0 1;0 0 0 1 1 0];
 A = 0.5*(A'+A);
-charA = '1111';
+charA = '111111';
 % B=[2 6 -1 -2;5 -1 2 3;-3 -4 1 10;5 -2 -3 8];
 %[V,D]=eig(A,B)
 alli = [];
@@ -15,16 +16,17 @@ allv = [];
 alld = [];
 epsx  = 0;
 epsxlambda = -1e-10;
- for iter = 1 : 2^4-1
+nv = 6;
+ for iter = 1 : 2^nv - 1
 % for iter = 8:8
-    F = dec2bin(iter, 4) == charA;
+    F = dec2bin(iter, nv) == charA;
     AF = A(F, F);
     [v,d] = eig(AF);
     [m,n] = size(d);
     Fn = sum(F);
     for i = 1:n
         if sum(v(:,i) >= 0) == Fn
-            v1 = zeros(4, 1);
+            v1 = zeros(nv, 1);
             v1(F) = v(:,i) ./ sum(v(:,i));
             alli = [alli, iter];
             alld = [alld d(i,i)];
@@ -35,6 +37,6 @@ epsxlambda = -1e-10;
     end
 end
 [alli;alld;allv]
-min(abs(alld))
-max(abs(alld))
+% min(abs(alld))
+% max(abs(alld))
     
