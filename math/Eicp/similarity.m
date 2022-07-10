d3563ea97a1fc035c621f25846bf3f6a0e12b8df
home <home@lb.com>
2022-07-10 20:19:59 +0800

    2022-07-10

--- math/Eicp/similarity.m
+++ math/Eicp/similarity.m
@@ -1,25 +1,51 @@
 function [flex, fed, fisd] = similarity(G,H)
- B = I;
+B = I;
 M = I*3;
 A = G;
+maxL = 0;
 for i = 1:2
     if i == 2
         A = H;
     end
-epsx = 0;
-epsxlambda = -1e-3;
-x1 = ones(n,1)/n;
-O=inv(B)*A;
-R=max(abs(eig(O))); %AµÄÆ×°ë¾¶  
-sigma0 = R + 0.01;
-[xa, iter, error] = allsqp(A, B, M, x1, sigma0, 0.1, 1e-5, 1e-8, 1000, 0);
-nx = size(xa,2);
-xd = []
-for i = 1 : nx
-    x = xa(:,i) ./ sum(xa(:,i));
-lambda = (x' * A * x) / (x' * B * x)-20;
-%xd = [xd [lambda; x]];
-xd = [xd lambda];
-%disp(['FQP_QUAD:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',ninfy='  num2str(sum((A - lambda * B) * x < epsxlambda))])
+    x1 = ones(n,1)/n;
+    O=inv(B)*A;
+    R=max(abs(eig(O))); %AµÄÆ×°ë¾¶
+    sigma0 = R + 0.01;
+    [xa, ~, ~] = allsqp(A, B, M, x1, sigma0, 0.1, 1e-5, 1e-8, 1000, 0);
+    nx = size(xa,2);
+    lambda = zeros(nx,1);
+    for j = 1 : nx
+        x = xa(:,j) ./ sum(xa(:,j));
+        lambda(j) = (x' * A * x) / (x' * B * x);
+    end
+     lambdaT = sort(uniquetol(lambda,1e-3),'descend');
+     mT = size(lambdaT,1);
+     if i == 1
+        lambdaA = zeros(mT,2);
+        lambdaA(:,1) = lambdaT;
+        maxL = mT;
+     elseif i == 2 && mT > maxL
+         lambdaTT = lambdaA(:,1);
+         lambdaA = zeros(mT,2);
+         lambdaA(1:maxL,1) = lambdaTT;
+         lambdaA(:,2) = lambdaT;
+         maxL = mT;
+     else
+         lambdaA(1:mT,2) = lambdaT;
+     end
 end
- Glambda = sort(uniquetol(xd,1e-3),'descend');
+simed = 0;
+simisd = 0;
+lex = 0;
+for i = 1:maxL
+    if abs(lambdaA(i,1)-lambdaA(i,2))>tol
+       if lex == 0
+          lex = i;
+       end
+       simed = simed + 2^(-i);
+       simisd = simisd + i^(-2);
+    end
+end
+flex = 1 - 1 / lex;
+fed = 1 - simed;
+fisd = 1-6 * pi * simisd;
--- math/Eicp/testAll.m
+++ math/Eicp/testAll.m
@@ -23,9 +23,10 @@ B=I+C'*C; %importdata('B3.txt'); %初始矩阵B
  n = 6;
 I=eye(n);
 %  A=[5 7 6 5;7 10 8 7;6 8 10 9;5 7 9 10];
-A = [0 1 0 1 0 0;1 0 0 1 0 0;0 0 0 1 0 0;1 1 1 0 1 1;0 0 0 1 0 1;0 0 0 1 1 0] ;
+A = [0 1 0 1 0 0;1 0 0 1 0 0;0 0 0 1 0 0;1 1 1 0 1 1;0 0 0 1 0 1;0 0 0 1 1 0] + 5*eye(n);
+%A = [0 1 0 0 0 0;1 0 1 1 0 0;0 1 0 1 1 0;0 1 1 0 1 0;0 0 1 1 0 1;0 0 0 0 1 0] + 5*eye(n) ;
  B = I;
-M = I*3;
+M = I;
 %  M=A; %A对称正定
 epsx = 0;
 epsxlambda = -1e-3;
@@ -33,14 +34,14 @@ x1 = ones(n,1)/n;
 O=inv(B)*A;
 R=max(abs(eig(O))); %A的谱半径  
 sigma0 = R + 0.01;
-[xa, iter, error] = allsqp(A, B, M, x1, sigma0, 0.1, 1e-5, 1e-8, 1000, 0);
+[xa, iter, error] = allsqp(A, B, M, x1, sigma0, 0.1, 1e-10, 1e-12, 1000, 0);
 nx = size(xa,2);
 xd = []
 for i = 1 : nx
     x = xa(:,i) ./ sum(xa(:,i));
-lambda = (x' * A * x) / (x' * B * x);
+lambda = (x' * A * x) / (x' * B * x)-5;
 %xd = [xd [lambda; x]];
 xd = [xd lambda];
 %disp(['FQP_QUAD:lambda=' num2str(lambda) ', ninfx=' num2str(sum(x<epsx)) ',ninfy='  num2str(sum((A - lambda * B) * x < epsxlambda))])
 end
- sort(uniquetol(xd,1e-3),'descend')'
+ sort(uniquetol(xd,0.01),'descend')'
