ef3e3d8daaeae247335beb39b2a86a3d7819f11c
home <home@lb.com>
2022-08-27 15:07:05 +0800

    2022-08-27

---
+++ math/nls/compareIFM_ArrGS.m
@@ -0,0 +1,84 @@
+clear
+clc
+debug = 1;
+%% 产生问题矩阵
+% 随机矩阵
+m = 3000;
+n = 300;
+
+A = 2 * rand(m , n)-1;
+b = 2 * rand(m , 1)-1;
+% b=A*ones(n,1);
+x0 = zeros(n , 1);
+% save('test2.mat','A','b','x0')
+%load('test.mat');
+% 二维矩阵
+% A = -[1,-1;-1,-1;0,1];b=-[0;-1;0];x0=[-1;0];
+% 不一致情况下的正解
+% x_exact=[1/2;1/3];
+
+
+%% 基于IFM的算法找到一个解
+maxit_LSQR = 3;
+r = b - A * x0;
+r(r<0) = 0;
+norm_r0 = norm(r);
+norm_g0 = norm(A'*r);
+fprintf('%s & %g & %g \n','最开始的目标函数和梯度', norm_r0, norm_g0);
+[x_exact, ~, ~, ~, ~] = IFM(A, b, x0,10, maxit_LSQR , 1e-10,[],debug);
+r = b - A * x_exact;
+r(r<0) = 0;
+norm_rexact = norm(r);
+norm_gexact = norm(A'*r);
+fprintf('%s & %g & %g \n','IFM解的目标函数值和梯度  ', norm_rexact, norm_gexact);
+x_exact=[];
+%% 参数的设定
+ maxit_IFM = 150;
+% 
+tol=1e-1;
+% tol=[];
+
+t=clock;
+[x_IFM,iter_IFM,error_IFM,xA_IFM,index_IFM] = IFM(A, b, x0, maxit_IFM, maxit_LSQR ,tol, x_exact,debug);
+tf_IFM=etime(clock,t);
+r = b - A * x_IFM;
+r(r<0) = 0;
+r_IFM = norm(r);
+g_IFM = norm(A'*r);
+fprintf('& %s & %s & %s & %s & %s \\\\\n', 'alg', 'norm(r_+)', 'norm(Ar_+)', 'iteration', 'time');
+fprintf('& %s & %g & %g & %d & %g \\\\\n','IFM', r_IFM, g_IFM,iter_IFM*(m+n)*maxit_LSQR,tf_IFM);
+
+
+%% GuassSeidel
+maxit_GS =35000;
+t=clock;
+ [x_GS,iter_GS,error_GS,xA_GS,index_GS] = GuassSeidelNE(A, b, x0,1.0,maxit_GS,tol,x_exact,debug);
+tf_GS=etime(clock,t);
+r = b - A * x_GS;
+r(r<0) = 0;
+r_GS = norm(r);
+g_GS = norm(A'*r);
+fprintf('& %s & %g & %g & %d & %g \\\\\n', 'GuassSeidel', r_GS, g_GS, iter_GS, tf_GS);
+
+maxit_FM = 1000;
+alpha = 1;
+maxit_gs = 1;
+t=clock;
+[x_FM,iter_FM,error_k,iter_k,index_k] = DFM(A, b, x0, maxit_FM,alpha,maxit_gs,tol, x_exact,debug);
+tf_FM=etime(clock,t);
+r = b - A * x_FM;
+r(r<0) = 0;
+r_FM = norm(r);
+g_FM = norm(A'*r);
+fprintf('& %s & %g & %g & %d & %g \\\\\n', 'FM', r_FM , g_FM, iter_FM*n, tf_FM);
+
+%% Rand
+maxit_Rand =35000;
+t=clock;
+[x_RGS,iter_RGS,error_RGS,xA_RGS,index_RGS] = randGSNE(A, b, x0,1.0,maxit_Rand,tol,x_exact,debug);
+tf_RGS=etime(clock,t);
+r = b - A * x_RGS;
+r(r<0) = 0;
+r_RGS = norm(r);
+g_RGS = norm(A'*r);
+fprintf('& %s & %g & %g & %d & %g \\\\\n', 'RGS', r_RGS, g_RGS, iter_RGS, tf_RGS);
---
+++ math/nls/urandGSNE.m
@@ -0,0 +1,97 @@
+function [x,iter,error_k,iter_k,index_k] = urandGSNE(A, b, x0,alpha ,maxit,tol,exactx,debug)
+%% 参数设定
+% 输入参数
+% A, b, x0 问题的系数矩阵和右边项 初始值
+% maxit,tol,exactx 最大迭代次数，容忍度，精确解
+% 输出参数
+% x iter 迭代最后的解, 实际迭代次数
+% error 如果没有精确解就存储每一次迭代的梯度2范数
+% iter_k 存计算梯度的迭代次数 为作图方便
+% index_k 如果是随机算法则存储随机选择的序列
+% exactx 精确值为空则将iter_k 放每次迭代的x值
+% debug 调试程序
+%%
+[m, n] = size(A);
+x = x0;
+iter = 0;
+
+%% 计算残差
+r = b - A * x;
+rs = r;
+r(r<0) = 0;
+norm_Ar = norm(A'*r);
+
+if isempty(exactx)
+    error_k = [norm_Ar];
+    iter_k =[0];
+else
+    error_k = [norm(x-exactx)];
+    iter_k =[x];
+end
+
+index_k=[0];
+% 因为测试终止条件需要矩阵乘以向量 为了避免每次迭代都去检测终止条件因此周期检测
+iter_test_stop = n;
+
+
+
+% Acol=sum(A.*A,1);
+
+Acol = [];
+index = [];
+
+
+% weight = Acol/sum(Acol);
+  for i = 1:n
+    Acol = [Acol,norm(A(:,i))^2];
+    index = [index,i];
+  end
+% % index=1:n;
+ weight = ones(1,n)*(1/n);
+% for i = 1:maxit
+%  pickedj_a(i) = randsample(index,1,true,weight);
+% end
+iter_index=1;
+
+for i = 1:maxit
+  %  pickedj=mod(i-1,n)+1;
+    pickedj=randsample(index,1,true,weight);
+    col = A(:, pickedj);
+    inc = alpha*( col' * r ) / Acol(pickedj);
+    x(pickedj) = x(pickedj) + inc;
+    rs = rs - inc*col;
+     r = rs;
+   %    if mod(iter,100)==0
+        r( r < 0) = 0;
+    %   r=(r+abs(r))/2;
+   %   end
+    iter = iter+1;
+    % 主要记录迭代过程中的值 用来调试
+    if mod(iter,iter_test_stop)==0
+        if ~isempty(tol) || debug
+            % 矩阵乘以向量 
+            Ar = A'*r;
+            e = norm(Ar);
+            normAr = norm(r);
+            % 如果有容忍度 即使没有到达最大迭代次数也终止
+            if ~isempty(tol)
+                if normAr < tol  || e < tol
+                    break;
+                end
+            end
+        end
+        % 用来记录迭代信息 可能会影响效率
+        if debug
+            if ~isempty(exactx)
+                e = norm(x-exactx);
+                iter_k =[iter_k x];
+            else
+                iter_k =[iter_k i];
+            end
+            error_k = [error_k,e];
+            index_k = [index_k,pickedj];
+        end
+    end
+end
+
+end
