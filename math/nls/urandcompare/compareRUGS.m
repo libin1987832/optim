ecd8baa76b6fedf23153ddde9993cfca75f13703
home <home@lb.com>
2022-09-20 10:08:24 +0800

    2022-09-20

---
+++ math/nls/urandcompare/compareRUGS.m
@@ -0,0 +1,30 @@
+clear
+clc
+debug = 0;
+%% 产生问题矩阵
+% 随机矩阵
+m = 10000;
+n = 100;
+A = 2*rand(m,n)-1;
+b = rand(m,1);
+
+x0 = zeros(n , 1);
+
+
+
+x_exact=[];
+%% 参数的设定
+ maxit_IFM = 150;
+maxit_LSQR = 3;
+tol=1e-1;
+% tol=[];
+maxit_GS =70000;
+t=clock;
+ [x_GS,iter_GS,error_GS,xA_GS,index_GS] = urandGSNE(A, b, x0,1.0,maxit_GS,tol,x_exact,debug);
+tf_GS=etime(clock,t);
+r = b - A * x_GS;
+r(r<0) = 0;
+r_GS = norm(r);
+g_GS = norm(A'*r);
+fprintf('& %s & %g & %g & %d & %g \\\\\n', 'urand', r_GS, g_GS, iter_GS, tf_GS);
+
