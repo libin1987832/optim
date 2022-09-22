efc9b58e9578c816fd4c2e7feeca6163633fc5b6
home <home@lb.com>
2022-09-22 11:07:34 +0800

    2022-09-22

---
+++ math/nls/compareIFM_GS2.m
@@ -0,0 +1,108 @@
+clear
+clc
+debug = 0;
+%% 产生问题矩阵
+% 随机矩阵
+
+r = 800;
+m = 10000;
+n = 2*r+1;
+% t=n;
+% n=m;
+% m=t;
+%% generate nodes
+%tj are drawing randomly from a uniform distribution in [0,1]
+t = rand(m,1);
+
+%then ordering by magnitude (since they are all positive, just sort)
+t = sort(t);
+% just to assign the size
+w = zeros(m,1); 
+
+%% generate A and b
+A = zeros(m,n);
+for j = 1:m
+    % dealing with special cases when reach the endpoints(nodes)
+    if j == 1
+        w(j) = (t(2)-t(end)-1)/2;
+    elseif j == m
+            w(j) = (t(1)+1-t(m-1))/2;
+        else
+            w(j) = (t(j+1)-t(j-1))/2;
+    end              
+    for k = -r:r
+       A(j,k+r+1) =sqrt(w(j))*exp(2*pi*1i*k*t(j));
+    end
+end  
+% A = A';
+% t=n;
+% n=m;
+% m=t;
+%A=real(A);
+% A = 2 * rand(m , n)-1;
+x = zeros(n,1);
+%% generate x 
+realx = randn(n,1);
+imgx = randn(n,1);
+for l = 1:n
+   x(l) = realx(l)+1i*imgx(l);
+end  
+b = A * x;
+% b=A*ones(n,1);
+x0 = zeros(n , 1);
+% save('test2.mat','A','b','x0')
+%load('test.mat');
+% 二维矩阵
+% A = -[1,-1;-1,-1;0,1];b=-[0;-1;0];x0=[-1;0];
+% 不一致情况下的正解
+% x_exact=[1/2;1/3];
+
+
+%% 基于IFM的算法找到一个解
+maxit_LSQR = 3;
+r = b - A * x0;
+r(r<0) = 0;
+norm_r0 = norm(r);
+norm_g0 = norm(A'*r);
+fprintf('%s & %g & %g \n','最开始的目标函数和梯度', norm_r0, norm_g0);
+% [x_exact, ~, ~, ~, ~] = IFM(A, b, x0,10, maxit_LSQR , 1e-10,[],debug);
+% r = b - A * x_exact;
+% r(r<0) = 0;
+% norm_rexact = norm(r);
+% norm_gexact = norm(A'*r);
+% fprintf('%s & %g & %g \n','IFM解的目标函数值和梯度  ', norm_rexact, norm_gexact);
+x_exact=[];
+%% 参数的设定
+ maxit_IFM = 150;
+% 
+tol=1e-1;
+% tol=[];
+
+t=clock;
+[x_IFM,iter_IFM,error_IFM,xA_IFM,index_IFM] = IFM(A, b, x0, maxit_IFM, maxit_LSQR ,tol, x_exact,debug);
+tf_IFM=etime(clock,t);
+r = b - A * x_IFM;
+r(r<0) = 0;
+r_IFM = norm(r);
+g_IFM = norm(A'*r);
+fprintf('& %s & %s & %s & %s & %s \\\\\n', 'alg', 'norm(r_+)', 'norm(Ar_+)', 'iteration', 'time');
+fprintf('& %s & %g & %g & %d & %g \\\\\n','IFM', r_IFM, g_IFM,iter_IFM*(m+n)*maxit_LSQR,tf_IFM);
+
+
+%% GuassSeidel
+maxit_GS =35000;
+t=clock;
+ [x_GS,iter_GS,error_GS,xA_GS,index_GS] = GuassSeidelNE(A, b, x0,1.0,maxit_GS,tol,x_exact,debug);
+tf_GS=etime(clock,t);
+r = b - A * x_GS;
+r(r<0) = 0;
+r_GS = norm(r);
+g_GS = norm(A'*r);
+fprintf('& %s & %g & %g & %d & %g \\\\\n', 'GuassSeidel', r_GS, g_GS, iter_GS, tf_GS);
+
+
+% & IFM & 0.21412 & 0.0872205 & 25221 & 0.089 \\
+% & GuassSeidel & 0.0346303 & 0.0161252 & 35000 & 1.521 \\
+% & FM & 0.149585 & 0.0727871 & 34170 & 0.653 \\
+% & RGS & 0.0224331 & 0.0125938 & 4623 & 0.288 \\
+
