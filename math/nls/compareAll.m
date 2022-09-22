4fa25b18eb5ec3bc4b40756663943fd237d4f43e
home <home@lb.com>
2022-09-22 17:07:29 +0800

    2022-09-22

--- math/nls/compareAll.m
+++ math/nls/compareAll.m
@@ -4,45 +4,50 @@ debug = 0;
 %% 产生问题矩阵
 % 随机矩阵
 
-r = 1000;
-m = 20000;
-n = 2*r+1;
-% t=n;
-% n=m;
-% m=t;
-%% generate nodes
-%tj are drawing randomly from a uniform distribution in [0,1]
-t = rand(m,1);
+% r = 1000;
+% m = 20000;
+% n = 2*r+1;
+% % t=n;
+% % n=m;
+% % m=t;
+% %% generate nodes
+% %tj are drawing randomly from a uniform distribution in [0,1]
+% t = rand(m,1);
+% 
+% %then ordering by magnitude (since they are all positive, just sort)
+% t = sort(t);
+% % just to assign the size
+% w = zeros(m,1); 
+% 
+% %% generate A and b
+% A = zeros(m,n);
+% for j = 1:m
+%     % dealing with special cases when reach the endpoints(nodes)
+%     if j == 1
+%         w(j) = (t(2)-t(end)-1)/2;
+%     elseif j == m
+%             w(j) = (t(1)+1-t(m-1))/2;
+%         else
+%             w(j) = (t(j+1)-t(j-1))/2;
+%     end              
+%     for k = -r:r
+%        A(j,k+r+1) =sqrt(w(j))*exp(2*pi*1i*k*t(j));
+%     end
+% end  
+% 
+% x = zeros(n,1);
+% %% generate x 
+% realx = randn(n,1);
+% imgx = randn(n,1);
+% for l = 1:n
+%    x(l) = realx(l)+1i*imgx(l);
+% end  
+% b = A * x;
 
-%then ordering by magnitude (since they are all positive, just sort)
-t = sort(t);
-% just to assign the size
-w = zeros(m,1); 
-
-%% generate A and b
-A = zeros(m,n);
-for j = 1:m
-    % dealing with special cases when reach the endpoints(nodes)
-    if j == 1
-        w(j) = (t(2)-t(end)-1)/2;
-    elseif j == m
-            w(j) = (t(1)+1-t(m-1))/2;
-        else
-            w(j) = (t(j+1)-t(j-1))/2;
-    end              
-    for k = -r:r
-       A(j,k+r+1) =sqrt(w(j))*exp(2*pi*1i*k*t(j));
-    end
-end  
-
-x = zeros(n,1);
-%% generate x 
-realx = randn(n,1);
-imgx = randn(n,1);
-for l = 1:n
-   x(l) = realx(l)+1i*imgx(l);
-end  
-b = A * x;
+m=10000;
+n=100;
+A = 2*rand(m,n);
+b = 2*rand(m,1);
 x0 = zeros(n , 1);
 
 
